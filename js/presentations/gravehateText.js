e9511002e2213911033b069edb1753dd4bbc33d0
Konrad Höffner <konrad.hoeffner@uni-leipzig.de>
2018-12-15 19:01:30 +0100

    Finish grave hate.

--- js/presentations/gravehateText.js
+++ js/presentations/gravehateText.js
@@ -1,6 +1,6 @@
 var gravehateText =
-`# Die 15 häufigsten Anti-Friedhofs-Karten in Commander
-Timetwister|Noxious Revival|Deathrite Shaman|Grafdigger's Cage|Rest in Peace
+`# Die besten Anti-Friedhofs-Karten in Commander
+Grafdigger's Cage|Rest in Peace
 Timetwister|High Tide|Timespiral|Turnabout|Frantic Search|Paradox Engine|Isochron Scepter|Dramatic Reversal|Dark Ritual|Noxious Revival
 Noxious Revival|Animate Dead|Elesh Norn, Grand Cenobite|Tormod's Crypt|Time Warp|Teferi, Temporal Archmage|The Chain Veil
 Deathrite Shaman|Verdant Catacombs|Birds of Paradise|Scavenging Ooze
@@ -8,11 +8,11 @@ Grafdigger's Cage|Animate Dead|Natural Order|Karador, Ghost Chieftain|Muldrotha,
 Rest in Peace|Animate Dead|Mikaeus, the Unhallowed|Glen Elendra Archmage|Skullclamp|Disenchant|Wear|Nature's Claim|Obsessive Search|Helm of Obedience
 Time Spiral|High Tide|Turnabout|Paradox Engine
 Faerie Macabre|Counterspell|Stifle|Trickbind|Survival of the Fittest|Protean Hulk|Karmic Guide|Grand Abolisher
-Nihil Spellbomb|Paradox Engine|Muldrotha, the Gravetide|
+Nihil Spellbomb|Paradox Engine|Muldrotha, the Gravetide|Yawgmoth's Will
 Scavenging Ooze|Survival of the Fittest|Birthing Pod|Yisan, the Wanderer Bard|Tymna, the Weaver
 Leyline of the Void|Lotus Petal|Flash|Protean Hulk|Helm of Obedience
 Memory's Journey|Animate Dead|Vampiric Tutor|Flash of Insight|Pact of Negation
-Tormod's Crypt|Jhoira, Weatherlight Captain
+Tormod's Crypt|Jhoira, Weatherlight Captain|Nihil Spellbomb|Karn, Silver Golem
 Bojuka Bog|Counterspell|Wasteland|Life from the Loam|Crop Rotation
 Containment Priest|Animate Dead|Living Death|Fact or Fiction|Natural Order|Opalescence|Replenish|Karador, Ghost Chieftain|Muldrotha, the Gravetide
 Diminishing Returns|High Tide|Turnabout||Time Spiral|Timetwister|Windfall
