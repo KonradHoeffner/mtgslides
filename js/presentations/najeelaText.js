2803745271d05207cd2da2412bf3d7cc2d426b27
Konrad Höffner <konrad.hoeffner@uni-leipzig.de>
2019-02-24 11:18:04 +0100

    Update najeelaText.js

--- js/presentations/najeelaText.js
+++ js/presentations/najeelaText.js
@@ -15,4 +15,20 @@ Grizzly Bears|Grizzly Bears|Grizzly Bears|Grizzly Bears|Grizzly Bears|Grizzly Be
 
 # Combo
 Nature's Will|Derevi, Empyrial Tactician|Druids' Repository|Sword of Feast and Famine|Bear Umbra|Cryptolith Rite|Phyrexian Altar
+# Zwei Arten von Combos: Die Landcombos und die Kreaturencombos
+
+## Landcombos
+# Landcombos ist es egal wieviele Kreaturen man hat, wichtig ist nur dass Gegner angegriffen werden oder Schaden bekommen
+# Landcombos Ein Gegner, Attacks vs Combat Damage
+Plains|Island|Swamp|Forest|Mountain|Nature's Will|Sword of Feast and Famine|Bear Umbra
+# Nature's Will, Zwei Gegner
+Nature's Will|Tundra|Taiga|Swamp
+# Nature's Will, Drei Gegner
+Nature's Will|City of Brass|Tundra
+
+# Kreaturcombos
+# Wichtig, wieviele Kreaturen man hat
+# Derevi: Kampfschaden, Druids' Repository: Angreifen, Cryptolith Rite: Ungetappt und ohne Einsatzverzögerung, Phyrexian Altar: opfern können
+Derevi, Empyrial Tactician|Druids' Repository|Cryptolith Rite|Phyrexian Altar
+
 `;
