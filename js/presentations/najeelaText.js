99a466c5c067e43a77f83a946f5770fdbbcb6eaf
Konrad Höffner <konrad.hoeffner@uni-leipzig.de>
2019-02-24 10:45:04 +0100

    Update najeelaText.js

--- js/presentations/najeelaText.js
+++ js/presentations/najeelaText.js
@@ -1,5 +1,18 @@
 var najeelaText =
 `Najeela, the Blade-Blossom
 
+# Passende Musik finden
+# Najeela, die ultimate Commanderin
+# Warum ist Najeela so gut? Die macht doch nur Schaden. Kreaturen sind doch blöd. Ich könnte doch
+# Tymna spielen und Karten ziehen. Edric spielen und noch mehr Karten ziehen. Zur spielen und Verzauberungen ins Spiel bringen.
 
+# Exponentieller Schaden
+# https://www.wolframalpha.com/input/?i=2%5En%2B2
+# Wie geht Summe dort?
+
+#  20 life vs 120 life
+Grizzly Bears|Grizzly Bears|Grizzly Bears|Grizzly Bears|Grizzly Bears|Grizzly Bears
+
+# Combo
+Nature's Will|Derevi, Empyrial Tactician|Druids' Repository|Sword of Feast and Famine|Bear Umbra|Cryptolith Rite|Phyrexian Altar
 `;
