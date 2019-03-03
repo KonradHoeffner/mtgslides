baab47c0a085d6cb18f8dbc0d41194f3b1a470f7
Konrad Höffner <konrad.hoeffner@uni-leipzig.de>
2019-03-03 11:01:58 +0100

    Create hanna2Text.js

---
+++ js/presentations/hanna2Text.js
@@ -0,0 +1,68 @@
+var hannaText =
+`# Paradox Hanna High Power EDH Deck
+Hanna, Ship's Navigator
+Hanna, Ship's Navigator|Paradox Engine|Isochron Scepter|Dramatic Reversal|Sol Ring|Aetherflux Reservoir
+
+# Combo
+
+## Isochron Scepter
+Isochron Scepter|Dramatic Reversal|Sol Ring|Sensei's Divining Top
+Isochron Scepter|Dramatic Reversal|Sol Ring|Mox Diamond|Copy Artifact|Brainstorm
+Isochron Scepter|Dramatic Reversal|Sol Ring|Mox Diamond|Copy Artifact|Swan Song
+Isochron Scepter|Dramatic Reversal|Sol Ring|Mox Diamond|Hanna Ship's Navigator|Aether Spellbomb|Phyrexian Furnace|Mind Stone
+
+# Paradox Engine
+Paradox Engine|Sol Ring|Isochron Scepter|Brainstorm
+Paradox Engine|Sensei's Divining Top|Scroll Rack|Sol Ring|Mox Diamond
+Paradox Engine|Hanna Ship's Navigator|Gilded Lotus|Sol Ring|Aether Spellbomb|Phyrexian Furnace
+Paradox Engine|Hanna Ship's Navigator|Gilded Lotus|Basalt Monolith|Mind Stone
+
+# In:
+Aetherflux Reservoir|Blind Obedience
+Isochron Scepter|Dramatic Reversal|Sol Ring|Aetherflux Reservoir
+Isochron Scepter|Dramatic Reversal|Mox Diamond|Blind Obedience
+Paradox Engine|Sol Ring|Mox Diamond|Aetherflux Reservoir|Future Sight|Memory Jar|Timetwister|Windfall
+
+# Out 
+Wurmcoil Engine|Lightning Greaves
+
+# Power Artifact
+Basalt Monolith|Grim Monolith|Power Artifact|Stroke of Genius|Braingeyser
+
+# Other Combos
+Sensei's Divining Top|Future Sight|Helm of Awakening 
+Opalescence|Parallax Wave|Parallax Tide
+High Tide|Time Spiral|Frantic Search|Treachery
+# Dazu:
+
+#Ramp and Mana Fixing
+Sol Ring|Mox Diamond|Mind Stone|Sky Diamond|Gilded Lotus|Lotus Petal|Grim Monolith|Basalt Monolith|Land Tax|Tithe
+# Mana Rocks both provide speed and win with Paradox Engine or Iso/Rev so the good ones are a no brainer and also improve Tezzereth
+# Problem 1: Zu wenig Ramp. Zu langsam.
+# Problem 2: Manchmal hat man Paradox Engine oder Iso/Rev aber nicht genug Artefaktmana
+# Problem 3: Nicht genug Ziele für 0 und 1 Mana für Tezzeret
+# Update
+# Rein
+Mana Crypt|Mana Vault|Azorius Signet|Talisman of Progress
+# Raus
+Voltaic Key
+# -3
+
+# Interaction
+## Removal 
+Wrath of God|Terminus|Swords to Plowshares|Parallax Wave|Treachery|Aura of Silence|Detention Sphere
+# raus: Detention Sphere
+# rein: 
+Cyclonic Rift|Humility|Ugin
+# -2 -3 -> -5
+
+## Counterspells
+# Bleibt
+Force of Will|Misdirection|Mana Drain|Counterspell|Arcane Denial|Spell Pierce|Swan Song|Decree of Silence
+# Decree raus
+# -4
+
+# Andere Karten raus
+Open the Vaults|Replenish
+
+`;
