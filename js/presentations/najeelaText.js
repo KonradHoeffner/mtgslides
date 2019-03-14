489457f4dd8a279860172f9b4c2984e9ed87a9c1
Konrad Höffner <konrad.hoeffner@uni-leipzig.de>
2019-03-14 13:35:04 +0100

    Update najeelaText.js

--- js/presentations/najeelaText.js
+++ js/presentations/najeelaText.js
@@ -6,10 +6,6 @@ var najeelaText =
 # Warum ist Najeela so gut? Die macht doch nur Schaden. Kreaturen sind doch blöd. Ich könnte doch
 # Tymna spielen und Karten ziehen. Edric spielen und noch mehr Karten ziehen. Zur spielen und Verzauberungen ins Spiel bringen.
 
-# Exponentieller Schaden
-# https://www.wolframalpha.com/input/?i=2%5En%2B2
-# Wie geht Summe dort?
-
 # beste Karten aus 5 Farben
 Demonic Tutor|Vampiric Tutor|Force of Will|Mana Drain|Mystic Remora|Brainstorm|Carpet of Flowers|Sylvan Library|Swords to Plowshares|Pyroblast
 # Schwarze Tutoren
@@ -20,6 +16,10 @@ Demonic Tutor|Vampiric Tutor|Force of Will|Mana Drain|Mystic Remora|Brainstorm|C
 
 #  20 life vs 120 life
 Grizzly Bears|Grizzly Bears|Grizzly Bears|Grizzly Bears|Grizzly Bears|Grizzly Bears
+
+# Exponentieller Schaden
+# https://www.wolframalpha.com/input/?i=2%5En%2B2
+# https://www.wolframalpha.com/input/?i=sum+2%5Ei%2B2,i%3D1+to+infinity
 
 # Combo
 Nature's Will|Derevi, Empyrial Tactician|Druids' Repository|Sword of Feast and Famine|Bear Umbra|Cryptolith Rite|Phyrexian Altar
@@ -38,5 +38,35 @@ Nature's Will|City of Brass|Tundra
 # Wichtig, wieviele Kreaturen man hat
 # Derevi: Kampfschaden, Druids' Repository: Angreifen, Cryptolith Rite: Ungetappt und ohne Einsatzverzögerung, Phyrexian Altar: opfern können
 Derevi, Empyrial Tactician|Druids' Repository|Cryptolith Rite|Phyrexian Altar
+
+# Najeela und 3 Combo Pieces hat, immer noch 96 Karten übrig, wie füllt man die?
+Demonic Tutor|Vampiric Tutor|Force of Will|Mana Drain|Mystic Remora|Brainstorm|Carpet of Flowers|Sylvan Library|Swords to Plowshares|Pyroblast
+
+# 14 Ramp spells: Najeela Turn 1 oder 2
+#  nur 4 Artefakte
+Mana Crypt|Sol Ring|Mox Diamond|Chrome Mox
+# 7 Kreaturen
+Birds of Paradise|Deathrite Shaman|Avacyn's Pilgrim|Elvish Mystic|Fyndhorn Elves|Llanowar Elves|Arbor Elf
+# 3 Enchantments
+Carpet of Flowers|Utopia Sprawl|Wild Growth
+# Ersetzung
+Mana Crypt|Bloom Tender
+
+# Cantrips
+Ponder|Brainstorm|Preordain
+
+# Interaktion
+# Counterspells
+# Hate Cards
+# Removal
+Grafdigger's Cage|Null Rod|Rest in Peace
+
+
+
+# Card Draw
+
+
+
+
 
 `;
