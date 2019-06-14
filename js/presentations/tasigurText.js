bfdbc3116e683653aa74c2375bded3742e2709cd
Konrad Höffner <konrad.hoeffner@uni-leipzig.de>
2019-06-14 22:11:08 +0200

    Create tasigurText.js

---
+++ js/presentations/tasigurText.js
@@ -0,0 +1,56 @@
+var muldrothaText =
+`Tasigur, the Golden Fang
+
+# Lands
+# 9
+Bloodstained Mire|Flooded Strand|Marsh Flats|Polluted Delta|Windswept Heath|Wooded Foothills|Misty Rainforest|Verdant Catacombs|Scalding Tarn
+# 9 -> 18
+Underground Sea|Tropical Island|Bayou|Watery Grave|Breeding Pool|Overgrown Tomb|Command Tower|City of Brass|Mana Confluence
+# 8 -> 29
+Island|Island|Island|Island|Forest|Forest|Forest|Forest|Swamp
+# 6 -> 33
+Cavern of Souls|Glacial Chasm|Cephalid Coliseum|Urborg, Tomb of Yawgmoth|Strip Mine|Bojuka Bog
+# Alternativen
+Tranquil Thicket|Barren Moor|Lonely Sandbar|Hinterland Harbor|Drowned Catacomb|Woodland Cemetery|Reflecting Pool|Sunken Ruins|Flooded Grove
+
+#Ramp 15
+# 7 Mana Rocks
+Sol Ring|Mana Crypt|Fellwar Stone|Mox Diamond|Simic Signet|Golgari Signet|Dimir Signet
+# 5 Dorks
+Birds of Paradise|Deathrite Shaman|Arbor Elf|Bloom Tender|Priest of Titania
+# 3 Enchantments
+Carpet of Flowers|Exploration|Utopia Spree
+# Alternativen
+Talisman of Dominance|Talisman of Curiosity|Talisman of Resilence|Thought Vessel|Mana Vault|Llanowar Elves|Fyndhorn Elves|Burgeoning
+
+#Card Draw
+Mystic Remora|Rhystic Study|Compost|Sylvan Library|Runic Armasaur|Timetwister|Notion Thief|Jace, the Mind Sculptor
+Fact or Fiction|Narset, Parter of Veils|Life from the Loam
+#Alternativen
+Dark Confidant|Verity Circle|Ad Nauseam|Nezahal, Primal Tide| Eldritch Evolution|Windfall
+
+#Cantrips
+Ponder|Brainstorm|Preordain
+
+#Interaction
+# Anti Kreatur
+Toxic Deluge|Reality Shift|Gilded Drake|Treachery
+# Counterspells
+Mana Drain|Force of Will|Counterspell|Narset's Reversal|Mana Leak|Mental Misstep|Swan Song| Negate
+# Andere 
+Cyclonic Rift|Abrupt Decay|Deathrite Shaman
+Crop Rotation|Bojuka Bog|Glacial Chasm
+# Alternativen
+Pernicious Deed|Nihil Spellbomb|Leyline of the Void|Dispel|Dimir Charm|Beast Within|Rapid Hybridization|Flusterstorm|Pact of Negation
+
+# Tutoren
+Demonic Tutor|Vampiric Tutor|Intuition|Mystical Tutor
+
+# Other
+Life from the Loam
+
+
+# Countering Tasigur
+Thrasios, Triton Hero|Tymna, the Weaver|Carpet of Flowers|Ashiok, Dream Render|Bojuka Bog
+
+`;
