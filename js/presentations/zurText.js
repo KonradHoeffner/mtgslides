ff5f971d8f660e09f4c9303e365a4bc88bef9202
Konrad Höffner <konrad.hoeffner@uni-leipzig.de>
2019-07-05 20:16:02 +0200

    Add Zur.

---
+++ js/presentations/zurText.js
@@ -0,0 +1,78 @@
+var zurText =
+`Zur the Enchanter
+
+Zur the Enchanter|Necropotence|Reliquary Tower|Empyrial Armor|Steel of the Godhead
+Zur the Enchanter|Necropotence|Reliquary Tower|Empyrial Armor|Steel of the Godhead|Lightning Greaves|Vanishing|Greater Auramancy|Diplomatic Immunity
+
+# Lands 32
+# 9
+Bloodstained Mire|Flooded Strand|Marsh Flats|Polluted Delta|Windswept Heath|Arid Mesa|Misty Rainforest|Verdant Catacombs|Scalding Tarn
+# 9 -> 18
+Underground Sea|Tundra|Scrubland|Watery Grave|Hallowed Fountain|Godless Shrine|Command Tower|City of Brass|Mana Confluence|Forbidden Orchard
+# 6 -> 25
+Island|Island|Island|Island|Plains|Swamp
+# 4 -> 29
+Ancient Tomb|Gemstone Mine|Mystic Gate|Urborg, Tomb of Yawgmoth
+# 3 -> 32 
+Cavern of Souls|Hall of the Bandit Lord|Reliquary Tower
+# Alternativen
+Island|Plains|Swamp|Strip Mine|Wasteland|Glacial Fortress|Drowned Catacomb|Reflecting Pool|Sunken Ruins|The Tabernacle at Pendrell Vale
+
+#Ramp & Fixing
+Sol Ring|Mana Crypt|Fellwar Stone|Mox Diamond|Thought Vessel|Azorius Signet|Dimir Signet|Talisman of Dominance|Talisman of Progress|Land Tax
+# Alternativen
+Mana Vault|Orzhov Signet|Talisman of Hierarchy|Chrome Mox
+
+#Setup für Zur
+Zur the Enchanter|Lightning Greaves|Hall of the Bandit Lord|Force of Will|Swan Song
+
+# Next Step
+Necropotence|Rhystic Study|Vanishing|Diplomatic Immunity|Rest in Peace|Arcane Laboratory|Aura of Silence
+
+#Card Advantage 
+Necropotence|Mystic Remora|Rhystic Study|Timetwister|Fact or Fiction|Future Sight|Sensei's Divining Top|Land Tax
+#Alternativen
+Verity Circle|Dark Confidant|Ad Nauseam|Bolas's Citadel
+#Rekursion
+Replenish|Open the Vaults
+
+# Tax
+Grand Arbiter Augustin IV|Aura of Silence|Aven Mindcensor|Containment Priest|Spirit of the Labyrinth|Arcane Laboratory
+
+#Card Quality
+Ponder|Brainstorm|Preordain|Sensei's Divining Top
+
+#Interaction
+# Anti Kreatur
+Supreme Verdict|Toxic Deluge|Wrath of God|Yahenni's Expertise|Gilded Drake|Treachery|Swords to Plowshares
+# Counterspells 12
+Mana Drain|Force of Will|Counterspell|Narset's Reversal|Mana Leak|Mental Misstep|Swan Song|Negate|Flusterstorm|Misdirection
+Spell Pierce|Counterbalance|Sensei's Divining Top
+# Anti Friedhof
+Grafdigger's Cage|Rest in Peace|Leyline of the Void
+# Andere 
+Grasp of Fate|Cyclonic Rift|Aura of Silence
+
+# Alternativen
+Nihil Spellbomb|Dispel|Pact of Negation|Damnation|Fatal Push|Dovin's Veto|Delay|Arcane Denial
+
+# Defense
+Solitary Confinement|Necropotence
+Solitary Confinement|Necropotence|Zur the Enchanter|Steel of the Godhead
+Solitary Confinement|Necropotence|Zur the Enchanter|Reliquary Tower|Empyrial Armor|Steel of the Godhead|Lightning Greaves|Vanishing|Greater Auramancy|Diplomatic Immunity
+
+# Tutoren
+Demonic Tutor|Vampiric Tutor|Enlightened Tutor|Mystical Tutor
+
+# Plan B
+Rest in Peace|Leyline of the Void|Helm of Obedience
+
+# 1v1 
+# Out 
+Mystic Remora|Rhystic Study|Swan Song|Leyline of the Void|Helm of Obedience
+# In
+Thoughtseize|Inquisition of Kozilek|Island|Plains|Strip Mine
+
+# Countering Zur 
+Swords to Plowshares|Liliana's Triumph|Plagecrafter|Imprisoned in the Moon|Aura Shards|Aura of Silence|Blind Obedience|Carpet of Flowers
+`;
