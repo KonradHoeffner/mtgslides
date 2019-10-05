2e54933f880328cd176e5ac0b962c45dab57fd43
Konrad Höffner <konrad.hoeffner@uni-leipzig.de>
2019-10-05 13:16:15 +0200

    Add Breya.

--- js/presentations/breyaText.js
+++ js/presentations/breyaText.js
@@ -1,5 +1,57 @@
 var breyaText =
 `Breya, Etherium Shaper
+Breya, Etherium Shaper | Plains | Island | Swamp | Mountain
+Breya, Etherium Shaper | Swords to Plowshares | Mystic Remora | Force of Will | Brainstorm | Demonic Tutor | Vampiric Tutor | Necropotence | Wheel of Fortune | Fire Covenant
+Breya, Etherium Shaper | Swords to Plowshares | Thopter | Thopter
+Breya, Etherium Shaper | Lava Spike | Thopter | Thopter | Sol Ring | Talisman of Dominance | Azorius Signet 
 
+Breya, Etherium Shaper | Thopter | Auriok Salvagers | Lion's Eye Diamond 
+Breya, Etherium Shaper | Entomb | Auriok Salvagers | Animate Dead | Reanimate | Gamble | Lion's Eye Diamond | Silence | Grand Abolisher
+Worldgorger Dragon | Animate Dead | Dance of the Dead | Necromancy | Command Tower
+Thought Scour | Auriok Salvagers | Lion's Eye Diamond | Animate Dead | Grizzly Bears | Doomsday | Brainstorm
+Breya, Etherium Shaper | Dramatic Reversal | Isochron Scepter | Sol Ring | Mox Diamond
+Breya, Etherium Shaper | Dramatic Reversal | Isochron Scepter | Sol Ring | Mox Diamond | Swan Song | Copy Artifact | Paradox Engine
+Thopter Foundry | Sword of the Meek | Ashnod's Altar | Krark-Clan Ironworks
+Breya, Etherium Shaper | Ashnod's Altar | Krark-Clan Ironworks | Eldrazi Displacer | Nim Deathmantle
+Breya, Etherium Shaper | Sword of the Meek | Thopter Foundry | Ashnod's Altar | Krark-Clan Ironworks | Eldrazi Displacer | Nim Deathmantle | Goblin Welder | Open the Vaults | Rest in Peace
 
+Breya, Etherium Shaper | Grasp of Darkness | Zur the Enchanter | Captain Sisay | Kess, Dissident Mage |  Grand Arbiter Augustin IV | Nekusar, the Mindrazer | Atraxa, Praetors' Voice | Selvala, Heart of the Wilds | Yidris, Maelstrom Wielder
+
+Breya, Etherium Shaper | Whitesun's Passage | Zurgo Bellstriker | Geist of Saint Traft | Nekusar, the Mindrazer | Kambal, Consul of Allocation | Ruric Thar, the Unbowed
+Breya, Etherium Shaper | Whitesun's Passage | Necropotence | Dark Confidant | Mana Crypt | Toxic Deluge | Fire Covenant | Talisman of Dominance|  City of Brass
+
+Badlands | Plateau | Scrubland | Tundra | Underground Sea | Volcanic Island | Command Tower | City of Brass | Mana Confluence | Exotic Orchard
+Arid Mesa | Bloodstained Mire | Flooded Strand | Marsh Flats | Misty Rainforest | Polluted Delta | Scalding Tarn | Verdant Catacombs | Windswept Heath | Wooded Foothills 
+Blood Crypt | Sacred Foundry | Godless Shrine | Hallowed Fountain | Watery Grave | Steam Vents | Island | Swamp | Plains | Mountain
+Cavern of Souls | Urborg, Tomb of Yawgmoth | Mishra's Workshop | Strip Mine | Gemstone Mine | Darkslick Shores | Darkwater Catacombs | Drowned Catacomb | Underground River | Morphic Pool 
+
+Sol Ring | Mana Crypt | Mox Diamond | Mana Vault | Mox Opal | Arcane Signet | Fellwar Stone | Talisman of Dominance | Dimir Signet | Chromatic Lantern
+Coalition Relic | Thran Dynamo | Grim Monolith | Basalt Monolith | Chrome Mox | Lotus Petal | Gilded Lotus
+
+Ponder | Brainstorm | Preordain | Mana Drain | Force of Will | Swords to Plowshares | Demonic Tutor | Vampiric Tutor | Toxic Deluge
+Mystic Remora | Rhystic Study | Swan Song
+Dark Confidant | Jace the Mind Sculptor | Daretti, Ingenious Iconoclast | Dack Fayden
+
+# 1v1 Tempo
+Inquisition of Kozilek | Thoughtseize | Daze | Force Spike | Mana Tithe | Mana Leak | Miscalculation | Lightning Bolt | Strip Mine | Armageddon
+
+# 1v1 Control Brian Weissman, Nathan Prawdzik -> + Multiplayer
+Mishra's Workshop | Gilded Lotus | Cryptic Command | Treachery | Mind Twist | Tezzeret the Seeker | Mystical Teachings | Necropotence | Winter Orb | Sword of Feast and Famine
+
+# Fast Combo
+Auriok Salvagers | Lion's Eye Diamond | Animate Dead | Worldgorger Dragon | Ad Nauseam | Pact of Negation | Silence | Chain of Vapor | Swan Song | Flusterstorm
+
+# Slow Combo
+Breya, Etherium Shaper | Sword of the Meek | Thopter Foundry | Ashnod's Altar | Krark-Clan Ironworks | Eldrazi Displacer | Nim Deathmantle | Goblin Welder | Open the Vaults | Rest in Peace
+
+# Superfriends
+Dack Fayden | Daretti, Ingenious Iconoclast | Jace, the Mind Sculptor | Tezzeret the Seeker | Teferi, Hero of Dominaria |  Karn Liberated | Ugin the Spirit Dragon | Toxic Deluge | Supreme Verdict | Winter Orb
+
+# Reanimator
+Jin-Gitaxias, Core Augur | Worldgorger Dragon | Entomb | Faithless Looting | Animate Dead | Dance of the Dead | Necromancy | Notion Thief | Windfall | Wheel of Fortune 
+
+# Casual ohne Combo
+Wurmcoil Engine | Thopter Foundry | Sword of the Meek | Treachery | Bribery | Ugin, the Spirit Dragon | Notion Thief | Timetwister | Bolas's Citadel |  Emrakul, the Promised End 
+
+Breya, Etherium Shaper | Thopter | Thopter
 `;
