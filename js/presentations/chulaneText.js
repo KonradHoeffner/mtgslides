492c22b6da456ce5540b42fe47129c439e74f5d2
Konrad Höffner <konrad.hoeffner@uni-leipzig.de>
2019-12-01 16:04:50 +0100

    start chulane

---
+++ js/presentations/chulaneText.js
@@ -0,0 +1,95 @@
+var chulaneText =
+`Chulane, Teller of Tales
+# https://www.mtgvault.com/kirdie2/decks/chulane/
+
+# Ramp spells: Chullen Turn 3 gut, follow up wichtig
+# Very creature heavy
+
+#  nur 4 Artefakte
+Sol Ring|Mana Crypt|Mox Diamond
+# 2 Enchantments, sprawl on basic for earthcraft
+Carpet of Flowers|Utopia Sprawl
+
+# Manadorks
+Bloom Tender|Joraga Treespeaker|Lotus Cobra|Priest of Titania
+Birds of Paradise|Avacyn's Pilgrim|Elvish Mystic|Fyndhorn Elves|Llanowar Elves|Arbor Elf|Noble Hierarch
+
+# Synergies and Combos
+
+# Threats
+Toxic Deluge|Wrath of God|Supreme Verdict|
+Treachery
+
+# old text
+
+# Protection
+Mana Drain|Counterspell||Force of Will|Negate|Mana Drain|Mana Leak|Mental Misstep|Spell Pierce|Swan Song
+Pyroblast|Red Elemental Blast
+# Not used
+
+
+# Hate Cards
+Rest in Peace|Grafdigger's Cage|Containment Priest|Aven Mindcensor|Null Rod|Containment Priest
+
+# 8 Removal
+Abrupt Decay|Cyclonic Rift|Fatal Push|Fire Covenant|Lightning Bolt|Nature's Claim|Swords to Plowshares|Wear // Tear
+
+# Manakurve niedrig -> Card Draw
+Compost|Mystic Remora|Sylvan Library|Rhystic Study|Skullclamp|Ad Nauseam|Necropotence|Urborg, Tomb of Yawgmoth
+Dark Confidant|Edric, Spymaster of Trest|Mindblade Render|Tymna the Weaver
+
+# Tutoren
+Demonic Tutor|Vampiric Tutor|Enlightened Tutor|Tainted Pact|Demonic Consultation
+
+# Bonuscombo
+Laboratory Maniac|Tainted Pact|Demonic Consultation
+
+# Anderes
+Dack Fayden|Secure the Wastes|Mother of Runes|Tithe
+
+# 32 Länder
+# natürlich 10 Duals, durch Shocklands ersetzbar
+Badlands|Bayou|Plateau|Savannah|Scrubland|Taiga|Tropical Island|Tundra|Underground Sea|Volcanic Island
+# 9 Fetches
+Arid Mesa|Bloodstained Mire|Flooded Strand|Marsh Flats|Misty Rainforest|Polluted Delta|Windswept Heath|Wooded Foothills|Verdant Catacombs
+# 6 grün blaue shock lands die man am anfang braucht für cantrips und mana dorks ohne Temple Garden
+Breeding Pool|Hallowed Fountain|Overgrown Tomb|Steam Vents|Stomping Ground|Watery Grave
+# Ersetzen durch beliebige ungetappte
+City of Brass|Command Tower|Exotic Orchard|Mana Confluence
+Gaea's Cradle|Urborg, Tomb of Yawgmoth|Forest
+
+# Nachfüllen
+Ad Nauseam|Necropotence
+
+# Backup Gameplan
+Secure the Wastes|Tainted Pact|Demonic Consultation|Laboratory Maniac
+
+## 1v1 15 Card Sideboard
+### Combo
+#### Out
+Nature's Will|Druids' Repository|Demonic Consultation|Laboratory Maniac
+#### In
+Sword of Feast and Famine|Bear Umbra|Godless Shrine|Temple Garden
+
+### Interaktion +2
+#### Out
+Swan Song|Red elemental blast|rest in peace|Grafdigger's cage|Containment Priest
+#### In
+Remand|Daze|Thoughtseize|Duress|Inquisition of Kozilek|Strip Mine|Wasteland
+
+### Card Draw -4 -> -2
+#### Out
+Compost|Mystic Remora|Rhystic Study|Necropotence|Ad Nauseam
+#### Rein
+Jace, the Mind Sculptor
+
+### Anderes +2 -> 0
+#### Out
+Negate
+#### In
+Stoneforge Mystic| Sword of Feast and Famine| Umezawa's Jitte
+
+# Dagegen spielen
+Chulane|Mana Drain|Swords to Plowshares|Toxic Deluge|Fire Covenant|Cyclonic Rift|Humility|Elesh Norn, Grand Cenobite|Umezawa's Jitte
+
+`;
