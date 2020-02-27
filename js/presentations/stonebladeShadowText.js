0a4a38bfd158bc2ea55f7c0f591b23e4db282280
Konrad Höffner <konrad.hoeffner@uni-leipzig.de>
2020-02-27 22:40:47 +0100

    Add stoneblade vs shadow.

---
+++ js/presentations/stonebladeShadowText.js
@@ -0,0 +1,36 @@
+var stonebladeShadowText =
+`Stoneforge Mystic|Death's Shadow
+
+Stoneforge Mystic|Batterskull|Sword of Feast and Famine|Snapcaster Mage|Mana Leak|Force of Negation|Path to Exile|Teferi, Time Raveler|Jace, the Mind Sculptor|Cryptic Command|
+Ice-Fang Coatl|Uro, Titan of Nature's Wrath|Once Upon a Time|Arcum's Astrolabe
+Ice-Fang Coatl|Uro, Titan of Nature's Wrath|Once Upon a Time|Arcum's Astrolabe|Supreme Verdict|Elspeth' Sun's Nemesis|Timely Reinforcements
+Spell Snare|Opt|Spell Queller|Snapcaster Mage|Jace, the Mind Sculptor
+
+Island|Plains|Flooded Strand|Misty Rainforest|Hallowed Fountain|Field of Ruin
+Glacial Fortress|Celestial Colonnade
+Forest|Breeding Pool|Temple Garden|Steam Vents|Sacred Foundry
+
+Celestial Purge|Timely Reinforcements|Supreme Verdict|Wrath of God|Mystical Dispute|Ashiok, Dream Render|Rest in Peace|Elspeth, Sun's Nemesis
+Celestial Purge|Timely Reinforcements|Supreme Verdict|Wrath of God|Mystical Dispute|Ashiok, Dream Render|Rest in Peace|Elspeth, Sun's Nemesis|Blood Moon|Veil of Summer
+
+Mana Leak|Force of Negation|Cryptic Command
+Spell Snare|Mana Leak|Force of Negation|Spell Queller|Cryptic Command
+
+Spell Snare|Temur Battle Rage|Snapcaster Mage|Drown in the Loch|Collective Brutality|Assassin's Trophy|Once Upon a Time|Tarmogoyf
+Spell Queller|Veil of Summer|Stubborn Denial|Fatal Push|Lightning Bolt|Gurmag Angler
+
+Toughtseize|Inquisition of Kozilek|Thought Scour|Gurmag Angler|Death's Shadow|Stubborn Denial|Stubborn Denial|Stubborn Denial
+Temur Battle Rage|Spell Snare|Mana Leak|Spell Queller|Force of Negation|Path to Exile
+The Royal Scions|Mana Leak|Spell Queller|Force of Negation
+
+Icefang Coatl|Temur Battle Rage|Mana Leak|Force of Negation|Path to Exile
+Icefang Coatl|The Royal Scions|Mana Leak|Force of Negation
+
+Opt|Arcum's Astrolabe|Ice-Fang Coatl
+
+Snapcaster Mage|Jace, the Mind Sculptor|Mystical Dispute
+
+#Island|Plains|Flooded Strand|Misty Rainforest|Hallowed Fountain|Glacial Fortress|Celestial Colonnade|Mystic Sanctuary|Field of Ruin
+#Island|Plains|Forest|Hallowed Fountain|Breeding Pool|Temple Garden|Mystic Sanctuary|Field of Ruin|Steam Vents 
+
+`;
