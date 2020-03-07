f860e927d9a768477c47f7dde004615526aec714
Konrad Höffner <konrad.hoeffner@uni-leipzig.de>
2020-03-07 18:55:50 +0100

    Add stoneblade mistakes.

---
+++ js/presentations/stonebladeMistakesText.js
@@ -0,0 +1,14 @@
+var stonebladeMistakesText =
+`Stoneforge Mystic|Batterskull|Sword of Feast and Famine|Snapcaster Mage|Mana Leak|Force of Negation|Path to Exile|Teferi, Time Raveler|Jace, the Mind Sculptor|Cryptic Command
+
+Island|Plains|Flooded Strand|Misty Rainforest|Hallowed Fountain|Field of Ruin|Glacial Fortress|Celestial Colonnade|Mystic Sanctuary
+Path to Exile
+Spell Snare|Mana Leak|Force of Negation|Spell Queller|Cryptic Command|Cavern of Souls
+Opt
+Snapcaster Mage
+Stoneforge Mystic|Batterskull|Sword of Feast and Famine
+Teferi, Time Raveler
+Jace, the Mind Sculptor
+
+Celestial Purge|Timely Reinforcements|Supreme Verdict|Mystical Dispute|Ashiok, Dream Render|Rest in Peace|Elspeth, Sun's Nemesis|Aether Gust|Distainful Stroke|Ceremonious Rejection
+`;
