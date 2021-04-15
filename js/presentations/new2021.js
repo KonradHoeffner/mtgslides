61329b4368152d5441be9441713e16eb08187f90
Konrad Höffner <konrad.hoeffner@uni-leipzig.de>
2021-04-15 22:51:09 +0200

    Start 2019-2021 overview.

---
+++ js/presentations/new2021.js
@@ -0,0 +1,40 @@
+// new cards from 2019 to 2021
+// Wizards hat das Tempo der Sets ganz schön gesteigert, alleine in diesem Jahr sind schon wieder 4 Sets in 4 Monaten herausgekommen
+// Gleichzeitig können viele aber kaum noch Commander spielen wegen Corona. Dazu kommt noch Arbeit, Kinder, ...
+// Man weiß dann gar nicht mehr, wo man anfangen soll, weil man die letzte Edition noch nicht kennt geschweige denn die Karten gefunden hat, die man daraus braucht und schon kommt die Neue heraus.
+// Darum hier eine Zusammenstellung der wichtigsten Karten und Entwicklungen der letzen zweieinhalb Jahre.
+// Erstmal beruhigend: Magic ist immer noch Magic und Commander ist immer noch Commander. Solange ihr nicht kompetitiv spielt funktionieren eure Decks wahrscheinlich immer noch.
+/*
+Oko
+Ich war ja noch nie ein Fan von Beast Within, aber Oko spiele ich in jedem blau-grünen Deck weil es einfach eine sehr flexible Antwort ist.
+Inzwischen gebanned in Standard, Pioneer, Modern und jetzt sogar Legacy. Aber im Multiplayer wesentlich besser handlebar und daher sehe ich keine Gefahr eines Bans im Commander. Ist von 35 € auf 10 € gesunken, ich habe einfach die 3 aus meinem Legacy RUG Delver deck in meine blau-grünen Commander Decks gepackt.
+*/
+
+var new2021 =
+`
+# Erstmal Historisch
+# 2018 Guilds of Ravnica
+Assassin's Trophy | Plaguecrafter
+# 2019
+# Ravnica Allegiance 2019-01
+Smothering Tithe | Mystic Remora | Rhystic Study | Wheel of Fortune
+# War of the Sparks 2019-05
+Narset, Parter of Veils | Ashiok, Dream Render | Teferi, Time Raveler | Narset's Reversal | Bolas's Citadel | Jace, Wielder of Mysteries
+# Modern Horizons  2019-06
+Talisman of Curiosity | Talisman of Hierarchy | Talisman of Resilence | Talisman of Conviction
+Force of Negation | Dovin's Veto | Generous Gift 
+# Commander 2019
+Dockside Extortionist
+# Throne of Eldrane 2019-10
+Urza, Lord High Artificer | Jace, Vryn's Prodigy |  | Winter Orb 
+Oko, Thief of Crowns | Beast Within
+Arcane Signet | Felwar Stone | Dimir Signet | Talisman of Progress | Mind Stone
+Opposition Agent | Hullbreacher
+# 2020
+# M20
+Veil of Summer
+# Commander 2020
+Fierce Guardianship 
+# Theros ?
+Thassa's Oracle | Jace, Wielder of Mysteries | Laboratory Maniac | Demonic Consultation | Tainted Pact
+`;
