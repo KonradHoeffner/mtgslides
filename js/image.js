6d30d3ef7953c3ab84348a57dec6158eec6efb82
Konrad Höffner <konrad.hoeffner@uni-leipzig.de>
2021-05-13 11:23:57 +0200

    Add prettier and apply it.

--- js/image.js
+++ js/image.js
@@ -1,47 +1,43 @@
 let cache = {};
 
-
 const format = "large";
 
-function loadCache()
-{
-	try
-	{
-		const s = localStorage.getItem("img"+format);
-		if(s) {cache = JSON.parse(s);}
+function loadCache() {
+	try {
+		const s = localStorage.getItem("img" + format);
+		if (s) {
+			cache = JSON.parse(s);
+		}
+	} catch (e) {
+		console.warn("Couldn't initialize cache.", e.name);
 	}
-	catch(e) {console.warn("Couldn't initialize cache.",e.name);}
 }
 
-function saveCache()
-{
-	try
-	{
-		localStorage.setItem("img"+format,JSON.stringify(cache));
+function saveCache() {
+	try {
+		localStorage.setItem("img" + format, JSON.stringify(cache));
+	} catch (e) {
+		console.warn("Couldn't save cache.", e.name);
 	}
-	catch(e) {console.warn("Couldn't save cache.",e.name);}
 }
 
-async function imageUrl(cardName)
-{
+async function imageUrl(cardName) {
 	let url = cards[cardName];
-	if(url) return url;
+	if (url) return url;
 
-	if((url=cache[cardName]))
-	{
-		console.warn(cardName+" not in index file, load from cache.");
+	if ((url = cache[cardName])) {
+		console.warn(cardName + " not in index file, load from cache.");
 		return url;
 	}
-	console.warn(cardName+" neither in index nor cache, load from scryfall.");
+	console.warn(cardName + " neither in index nor cache, load from scryfall.");
 
-	const uri = 'https://api.scryfall.com/cards/search?q='+encodeURIComponent(`!"${cardName}"++not:reprint`);
-	const json = await fetch(uri).then(res=>res.json());
-	if(!json||!json.data)
-	{
-		console.warn("Could not find image for card "+cardName);
+	const uri = "https://api.scryfall.com/cards/search?q=" + encodeURIComponent(`!"${cardName}"++not:reprint`);
+	const json = await fetch(uri).then((res) => res.json());
+	if (!json || !json.data) {
+		console.warn("Could not find image for card " + cardName);
 		return null;
 	}
 	url = json.data[0].image_uris[format];
-	cache[cardName]=url;
+	cache[cardName] = url;
 	return url;
 }
--- js/presentation.js
+++ js/presentation.js
@@ -1,29 +1,28 @@
-class Presentation
-{
-	constructor(slides)
-	{
+class Presentation {
+	constructor(slides) {
 		this.slides = slides;
 		console.log(slides);
 	}
 
-	static async parse(text)
-	{		
+	static async parse(text) {
 		loadCache();
 		const slides = [];
 		const lines = text.split("\n");
-		for(let line of lines)
-		{
+		for (let line of lines) {
 			line = line.trim();
-			if(!line||line.startsWith("#")) {continue;} // ignore comments
+			if (!line || line.startsWith("#")) {
+				continue;
+			} // ignore comments
 			cardNames = line.split("|");
 			const slide = [];
 			slides.push(slide);
 
-			for(let cardName of cardNames)
-			{
+			for (let cardName of cardNames) {
 				cardName = cardName.trim();
-				if(!cardName) {continue;}
-				const card = {name: cardName};
+				if (!cardName) {
+					continue;
+				}
+				const card = { name: cardName };
 				card.img = await imageUrl(cardName);
 				slide.push(card);
 			}
@@ -32,48 +31,46 @@ class Presentation
 		return new Presentation(slides);
 	}
 
-	sleep(time)
-	{
- 		return new Promise((resolve) => setTimeout(resolve, time));
+	sleep(time) {
+		return new Promise((resolve) => setTimeout(resolve, time));
 	}
 
-	start()
-	{
+	start() {
 		this.shown = [];
 		this.slideNr = -1;
 		this.next();
 	}
 
-	next()
-	{
-		if(this.slideNr>=this.slides.length-1) {return false;}
+	next() {
+		if (this.slideNr >= this.slides.length - 1) {
+			return false;
+		}
 		this.show(++this.slideNr);
 	}
 
-	prev()
-	{
-		if(this.slideNr<=0) {return false;}
+	prev() {
+		if (this.slideNr <= 0) {
+			return false;
+		}
 		this.show(--this.slideNr);
 	}
 
-
-	show(slideNr)
-	{
-			for(const element of this.shown)
-			{
-				document.body.removeChild(element);
-			}
-			this.shown.length=0;
-			const slide = this.slides[slideNr];
-			console.log(slide);
-			if(!slide) {return false;}
-			for(const card of slide)
-			{
-				const img = document.createElement("IMG");
-				img.src = card.img;
-				img.classList.add(slide.length>2?"card50":"card");
-				document.body.appendChild(img);
-				this.shown.push(img);
-			}
+	show(slideNr) {
+		for (const element of this.shown) {
+			document.body.removeChild(element);
+		}
+		this.shown.length = 0;
+		const slide = this.slides[slideNr];
+		console.log(slide);
+		if (!slide) {
+			return false;
+		}
+		for (const card of slide) {
+			const img = document.createElement("IMG");
+			img.src = card.img;
+			img.classList.add(slide.length > 2 ? "card50" : "card");
+			document.body.appendChild(img);
+			this.shown.push(img);
+		}
 	}
 }
