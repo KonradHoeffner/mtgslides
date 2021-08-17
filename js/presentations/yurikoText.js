ad88fefec2333b2dcdb95df59a81f1239c97d82e
Konrad Höffner <konrad.hoeffner@uni-leipzig.de>
2021-08-17 22:09:27 +0200

    More Yuriko.

--- js/presentations/yurikoText.js
+++ js/presentations/yurikoText.js
@@ -1,6 +1,7 @@
 var yurikoText = `Yuriko, the Tiger's Shadow
-Yuriko, the Tiger's Shadow | Ingenious Infiltrator | Ninja of the Deep Hours | Sakashima's Student | Mistblade Shinobi | Skullsnatcher | Changeling Outcast | Universal Automaton
-Faerie Seer | Fourth Bridge Prowler | Gingerbrute | Mothdust Changeling | Ornithopter | Phyrexian Walker | Spectral Sailor | Wingcrafter | Will-O'-the-Wisp
+Yuriko, the Tiger's Shadow | Ingenious Infiltrator | Ninja of the Deep Hours | Sakashima's Student | Mistblade Shinobi | Mist-Syndicate Naga | Skullsnatcher | Fallen Shenobi
+Changeling Outcast | Universal Automaton | Mothdust Changeling | Faerie Seer | Fourth Bridge Prowler | Gingerbrute | Ornithopter | Spectral Sailor | Wingcrafter | Siren Stormtamer
+Phyrexian Walker | Will-O'-the-Wisp | Flying Men
 # Example Hands
 Mystical Tutor | Island | Swamp | Changeling Outcast | Misdirection | Rhystic Study | Submerge
 Submerge | Marsh Flats | Contagion | Umezawa's Jitte | Wingcrafter | Phantasmal Image | Vampiric Tutor
@@ -11,13 +12,15 @@ Vampiric Tutor | Island | Phyrexian Walker | Umezawa's Jitte | Urborg, Tomb of Y
 Underground Sea | Command Tower | Bloodstained Mire | Flooded Strand | Marsh Flats | Misty Rainforest | Polluted Delta | Scalding Tarn | Verdant Catacombs | Watery Grave
 Island | Swamp | Urborg, Tomb of Yawgmoth | Strip Mine | Gemstone Caverns | Darkslick Shores |Drowned Catacomb | Underground River
 
+Agadeem's Awakening | Agadeem, the Undercrypt
+
 Sol Ring | Mana Crypt | Mox Diamond | Chrome Mox
 
 Ponder | Brainstorm | Preordain | Mana Drain | Force of Will | Demonic Tutor | Vampiric Tutor
 Mystic Remora | Rhystic Study | Swan Song
 
-# 1v1 Tempo
-Inquisition of Kozilek | Thoughtseize | Daze | Force Spike | Fatal Push | Mana Leak | Miscalculation | Strip Mine
+# 1v1
+Jace, the Mind Sculptor | Baleful Strix | Daze | Force Spike | Fatal Push | Mana Leak | Miscalculation | Strip Mine | Snapcaster Mage | True-Name Nemesis
 # cEDH
 Thassa's Oracle | Demonic Consultation | Tainted Pact | Imperial Seal | Treachery | Umezawa's Jitte | Time Warp | Smoke Shroud
 `;
