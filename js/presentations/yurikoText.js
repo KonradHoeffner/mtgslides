ca930e6967a7fc627dcff345e286868e2877fe66
Konrad Höffner <konrad.hoeffner@uni-leipzig.de>
2021-08-17 21:16:03 +0200

    Add prettier config file and use it on index.html. Start Yuriko.

---
+++ js/presentations/yurikoText.js
@@ -0,0 +1,23 @@
+var yurikoText = `Yuriko, the Tiger's Shadow
+Yuriko, the Tiger's Shadow | Ingenious Infiltrator | Ninja of the Deep Hours | Sakashima's Student | Mistblade Shinobi | Skullsnatcher | Changeling Outcast | Universal Automaton
+Faerie Seer | Fourth Bridge Prowler | Gingerbrute | Mothdust Changeling | Ornithopter | Phyrexian Walker | Spectral Sailor | Wingcrafter | Will-O'-the-Wisp
+# Example Hands
+Mystical Tutor | Island | Swamp | Changeling Outcast | Misdirection | Rhystic Study | Submerge
+Submerge | Marsh Flats | Contagion | Umezawa's Jitte | Wingcrafter | Phantasmal Image | Vampiric Tutor
+Cursed Totem | Island | Prismatic Vista | Treachery | Swamp | Temporal Trespass | Stifle
+Stifle | Gingerbrute | Grafdigger's Cage | Island | Island | Island | Chrome Mox
+Vampiric Tutor | Island | Phyrexian Walker | Umezawa's Jitte | Urborg, Tomb of Yawgmoth | Swamp | Fearie Seer
+
+Underground Sea | Command Tower | Bloodstained Mire | Flooded Strand | Marsh Flats | Misty Rainforest | Polluted Delta | Scalding Tarn | Verdant Catacombs | Watery Grave
+Island | Swamp | Urborg, Tomb of Yawgmoth | Strip Mine | Gemstone Caverns | Darkslick Shores |Drowned Catacomb | Underground River
+
+Sol Ring | Mana Crypt | Mox Diamond | Chrome Mox
+
+Ponder | Brainstorm | Preordain | Mana Drain | Force of Will | Demonic Tutor | Vampiric Tutor
+Mystic Remora | Rhystic Study | Swan Song
+
+# 1v1 Tempo
+Inquisition of Kozilek | Thoughtseize | Daze | Force Spike | Fatal Push | Mana Leak | Miscalculation | Strip Mine
+# cEDH
+Thassa's Oracle | Demonic Consultation | Tainted Pact | Imperial Seal | Treachery | Umezawa's Jitte | Time Warp | Smoke Shroud
+`;
