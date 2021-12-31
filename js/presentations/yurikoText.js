39797f72a7d891131a1f5ee969a5beeb974c8c76
Konrad Höffner <konrad.hoeffner@uni-leipzig.de>
2021-12-31 14:33:25 +0100

    Finish first version of Yuriko.

--- js/presentations/yurikoText.js
+++ js/presentations/yurikoText.js
@@ -1,7 +1,53 @@
 var yurikoText = `Yuriko, the Tiger's Shadow
-Yuriko, the Tiger's Shadow | Ingenious Infiltrator | Ninja of the Deep Hours | Sakashima's Student | Mistblade Shinobi | Mist-Syndicate Naga | Skullsnatcher | Fallen Shenobi
-Changeling Outcast | Universal Automaton | Mothdust Changeling | Faerie Seer | Fourth Bridge Prowler | Gingerbrute | Ornithopter | Spectral Sailor | Wingcrafter | Siren Stormtamer
-Phyrexian Walker | Will-O'-the-Wisp | Flying Men
+# Enablers
+Changeling Outcast | Mothdust Changeling | Universal Automaton | Ornithopter | Phyrexian Walker | Memnite | Faerie Seer | Wingcrafter |  Gingerbrute |  Spectral Sailor
+Sidisi's Faithful | Thieve's Guild Enforcer | Fourth Bridge Prowler | Hope of Ghirapur | Mausoleum Wanderer | Stonecoil Serpent | Siren Stormtamer | Gudul Lurker | Will-O'-the-Wisp | Flying Men
+# Ninjas
+Yuriko, the Tiger's Shadow | Ingenious Infiltrator | Ninja of the Deep Hours | Sakashima's Student | Mistblade Shinobi | Mist-Syndicate Naga | Skullsnatcher | Fallen Shinobi |  Azra Smokeshaper | Metallic Mimic
+Higure, the Still Wind | Silent-Blade Oni | Ink-Eyes, Servant of Oni | Phantom Ninja | Ninja of the New Moon | Okiba-Gang Shinobi
+# Combat Phase Image https://drive.google.com/file/d/1Vrk8BcbeWiUJ5ZVpBv7pRcy-_nGsmsuh/view?usp=sharing
+# Example
+Swamp | Changeling Outcast
+Swamp | Island | Changeling Outcast | Yuriko, the Tiger's Shadow | Smoke Shroud
+Swamp | Phyrexian Walker
+Swamp | Island | Phyrexian Walker| Yuriko, the Tiger's Shadow
+Swamp | Island | Island | Phyrexian Walker | Yuriko, the Tiger's Shadow | Ingenious Infiltrator
+# Tricks with Ninjutsu
+Ingenious Infiltrator | Lightning Bolt | Mistblade Shinobi | Sakashima's Student | Azra Smokeshaper 
+# Lands
+#Underground Sea | Command Tower | Bloodstained Mire | Flooded Strand | Marsh Flats | Misty Rainforest | Polluted Delta | Scalding Tarn | Verdant Catacombs | Watery Grave
+Island | Swamp | Underground Sea | Command Tower |  Watery Grave | Polluted Delta | Prismatic Vista | Gemstone Caverns | Underground River | Clearwater Pathway
+Agadeem's Awakening | Agadeem, the Undercrypt | Sea Gate, Reborn | Sea Gate Restoration | Urborg, Tomb of Yawgmoth | Darkslick Shores | Sunken Ruins | Shizo, Death's Store House | Strip Mine | Morphic Pool
+# Island | Swamp | Urborg, Tomb of Yawgmoth | Strip Mine | Gemstone Caverns | Darkslick Shores |Drowned Catacomb | Underground River
+Dimir Aqueduct | Temple of Deceit | Dismal Backwater | Jwar Isle Refuge | Fetid Pools | Path of Ancestry | Fabled Passage | Evolving Wilds | Terramorphic Expanse | Myriad Landscape
+# Mana Rocks
+Mox Amber | Chrome Mox | Mox Diamond | Sol Ring | Mana Crypt | Lotus Petal | Dark Ritual
+Arcane Signet | Dimir Signet | Talisman of Dominance | Mind Stone | Thought Vessel | Fellwar Stone
+# Utility Creatures
+Dauthi Voidwalker | Tesuko Umezawa, Fugitive | Grazilaxx, Illithid Scholar | Gilded Drake | Phantasmal Image | Opposition Agent | Grim Hireling | Baleful Strix | Draco | Emrakul, the Promised End
+# Utility
+Cover of Darkness | Scroll Rack | Umezawa's Jitte |  Steal Enchantment | Smoke Shroud | Treachery | Scroll Rack | Lim-Dûl's Vault |
+# Stax
+Dauthi Voidwalker | Opposition Agent | Back to Basics | Null Rod | Cursed Totem | Grafdigger's Cage
+#  Card Draw, Cantrips and Tutors
+Demonic Tutor | Vampiric Tutor | Mystical Tutor | Ponder | Brainstorm |  Mystic Remora | Rhystic Study | Necropotence | Treasure Cruise | Dig Through Time
+# Counterspells
+Fierce Guardianship | Force of Will | Mana Drain | Counterspell | Force of Negation | Mental Misstep | Misdirection | Spell Pierce | Swan Song  | Abjure
+# Removal
+Snuff Out | Dismember | Contagion | Treachery | Submerge | Cyclonic Rift | Gilded Drake | Liliana's Triumph | Boomerang | Umezawa's Jitte
+# Extra Turns
+Time Warp | Temporal Mastery | Temporal Manipulation | Temporal Trespass
+
+# 1v1 out
+Mystic Remora | Rhystic Study | Swan Song | Morphic Pool | Grafdigger's Cage | Null Rod | Cursed Totem | Lotus Petal | Dark Ritual | Submerge
+# 1v1 in
+Jace, the Mind Sculptor | Baleful Strix | Daze | Force Spike | Fatal Push | Mana Leak | Miscalculation | Strip Mine | Snapcaster Mage | True-Name Nemesis
+
+# cEDH out
+Swamp | Island | Emrakul, the Promised End | Treachery | Scroll Rack | Umezawa's Jitte | Time Warp | Temporal Manipulation | Temporal Mastery | Smoke Shroud
+# cEDH in
+Thassa's Oracle | Demonic Consultation | Tainted Pact | Doomsday | Gush | Imperial Seal | Diablolic Intent | Deadly Rollick | Flusterstorm | Chains of Mephistopheles
+
 # Example Hands
 Mystical Tutor | Island | Swamp | Changeling Outcast | Misdirection | Rhystic Study | Submerge
 Submerge | Marsh Flats | Contagion | Umezawa's Jitte | Wingcrafter | Phantasmal Image | Vampiric Tutor
@@ -9,18 +55,4 @@ Cursed Totem | Island | Prismatic Vista | Treachery | Swamp | Temporal Trespass 
 Stifle | Gingerbrute | Grafdigger's Cage | Island | Island | Island | Chrome Mox
 Vampiric Tutor | Island | Phyrexian Walker | Umezawa's Jitte | Urborg, Tomb of Yawgmoth | Swamp | Fearie Seer
 
-Underground Sea | Command Tower | Bloodstained Mire | Flooded Strand | Marsh Flats | Misty Rainforest | Polluted Delta | Scalding Tarn | Verdant Catacombs | Watery Grave
-Island | Swamp | Urborg, Tomb of Yawgmoth | Strip Mine | Gemstone Caverns | Darkslick Shores |Drowned Catacomb | Underground River
-
-Agadeem's Awakening | Agadeem, the Undercrypt
-
-Sol Ring | Mana Crypt | Mox Diamond | Chrome Mox
-
-Ponder | Brainstorm | Preordain | Mana Drain | Force of Will | Demonic Tutor | Vampiric Tutor
-Mystic Remora | Rhystic Study | Swan Song
-
-# 1v1
-Jace, the Mind Sculptor | Baleful Strix | Daze | Force Spike | Fatal Push | Mana Leak | Miscalculation | Strip Mine | Snapcaster Mage | True-Name Nemesis
-# cEDH
-Thassa's Oracle | Demonic Consultation | Tainted Pact | Imperial Seal | Treachery | Umezawa's Jitte | Time Warp | Smoke Shroud
 `;
