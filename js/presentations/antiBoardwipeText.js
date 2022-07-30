b8490cb83e463fa4dbb2903dbf9df3ff467f92bb
Konrad Höffner <konrad.hoeffner@uni-leipzig.de>
2022-07-30 12:16:52 +0200

    Add anti board wipe presentation.

---
+++ js/presentations/antiBoardwipeText.js
@@ -0,0 +1,38 @@
+var antiBoardwipeText = `# How to protect against creature boardwipes
+Toxic Deluge | Cyclonic Rift | | Damn | Farewell | Blasphemous Act | Supreme Verdict | Fire Covenant | Pernicious Deed | Culling Ritual | Toxrill, the Corrosive 
+Teferi, Temporal Archmage | Jace, Vryn's Prodigy | Urza, Lord High Artificer | Zur the Enchanter | Tasigur, the Golden Fang | Mayael the Anima | Krenko, Mob Boss | Chulane, Teller of Tales | Animar, Soul of Elements | Sliver Queen
+# Finish with Burn
+Jackal Pup | Ragavan, Nimble Pilferer | Ball Lightning | Lightning Bolt | Fireblast | Torbran, Thane of Red Fell | Vial Smasher the Fierce | Ruric Thar, the Unbowed | Yuriko, the Tiger's Shadow | Kambal, Consul of Allocation
+# Fast Combat Damage
+Ghazban Ogre | Rogue Elephant | Elvish Spirit Guide | Spectral Bears | Giant Growth | Craterhoof Behemoth | Triumph of the Hordes | Kamahl, Heart of Krosa | Elesh Norn, Grand Cenobite | Najeela, the Blade-Blossom
+# Fast Combo
+Thassa's Oracle | Demonic Consultation | Isochron Scepter | Dramatic Reversal | Underworld Breach | Ad Nauseam | Rocco, Cabaretti Caterer | Food Chain | Kinnan, Bonder Prodigy | Basalt Monolith
+# Indestructible
+Toski, Bearer of Secrets | Ulamog, the Infinite Gyre | Ulamog, the Ceaseless Hunger | Blightsteel Colossus | Koma, Cosmos Serpent | Toxic Deluge | Toxrill, the Corrosive | Elesh Norn, Grand Cenobite | Farewell | Cyclonic Rift
+# ETB or Dies Trigger
+Eternal Witness | Ranger-Captain of Eos | Skyclave Apparition | Gilded Drake | Dockside Extortionist | Thassa's Oracle | Sakura-Tribe Elder | Reclamation Sage | Baleful Strix | Solemn Simulacrum
+# Generate Value
+Tymna the Weaver | Esper Sentinel | Augur of Autumn | Future Sight | Yisan, the Wanderer Bard | Duskwatch Recruiter | Lurking Predators | Sun Titan | Grim Hireling | Professional Face-Breaker 
+# Vulnerable Boards
+LLanowar Elves | Fyndhorn Elves | Priest of Titania | Bloom Tender | Birds of Paradise | Deathrite Shaman | Lotus Cobra  | Faeburrow Elder | Seedborn Muse | Dauthi Voidwalker
+# Other Card Types
+#Null Rod | Vivien, Monster's Advocate 
+# Graveyard Recursion
+Sheoldred, Whispering One | Animate Dead | Living Death | Yawgmoth's Will |  Mikaeus, the Unhallowed | Patriarch's Bidding | Sun Titan | Karmic Guide | Reveillark | Underworld Breach 
+# Can't cast
+Archon of Valor's Reach | Yasharn, Implacable Earth | Sanctum Prelate | Chalice of the Void | Void Winnower | Gaddock Teeg | Damn | Cyclonic Rift | Toxic Deluge | Toxrill, the Corrosive
+# Counterspells
+Force of Will | Mana Drain | Force of Negation | Fierce Guardianship | Flusterstorm | Swan Song | Dovin's Veto | An Offer you Can't Refuse | Dovescape | Kozilek, the Great Distortion
+# Other
+#Ashaya, Soul of the Wild | Quirion Ranger | Kamahl, Hearth of Krosa | Whisperwood Elemental | Wirewood Symbiote | 
+# Spot Removal Protection
+# Mother of Runes | Giver of Runes | Shalai, Voice of Plenty | Saryth, the Viper's Fang | Sylvan Safekeeper | 
+#Dauntless Escort
+Glorious Protector | Guardian of Faith | Teferi's Protection | Flawless Maneuver | Akroma's Will | Selfless Spirit | Avacyn, Angel of Hope | Heroic Intervention | Sliver Hivelord | Eldrazi Monument
+# Examples
+Chulane, Teller of Tales | Force of Will | Mana Drain | Force of Negation | Fierce Guardianship | Flusterstorm | Swan Song | Dovin's Veto | Future Sight | Augur of Autumn
+Tymna the Weaver | Kamahl, Heart of Krosa | Archon of Valor's Reach | Yasharn, Implacable Earth | Chord of Calling | Glorious Protector | Sheoldred, Whispering One | Animate Dead | Living Death | Augur of Autumn
+Yeva, Nature's Herald | Scryb Ranger | Chord of Calling | Emergence Zone | Vivien, Champion of the Wilds | Lurking Predators | Kogla, the Titan Ape | Whisperwood Elemental | Ashaya, Soul of the Wild | Wirewood Symbiote
+Magda, Brazen Outlaw | Clock of Omens | Universal Automaton | Tibalt's Trickery | Mechtitan Core | Blightsteel Colossus | Worldgorger Dragon
+Toxic Deluge | Cyclonic Rift | | Damn | Farewell | Blasphemous Act | Supreme Verdict | Fire Covenant | Pernicious Deed | Culling Ritual | Toxrill, the Corrosive 
+`;
