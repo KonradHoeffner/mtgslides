cc196873dd11f12e6f99f72f7e80d6722c665d3a
Konrad Höffner <konrad.hoeffner@uni-leipzig.de>
2022-08-26 23:24:57 +0200

    Add anti land.

---
+++ js/presentations/antiLandText.js
@@ -0,0 +1,20 @@
+var antiLandText = `# How to counter lands matter decks
+Aesi, Tyrant of Gyre Strait | The Gitrog Monster | Tatyova, Benthic Druid | Uro, Titan of Nature's Wrath | Lord Windgrace |  Omnath, Locus of Rage | Omnath, Locus of Creation | Azusa, Lost but Seeking | Titania, Protector of Argoth | Nissa, Vastwood Seer
+Exploration | Nature's Lore | Cultivate | Ramunap Excavator | Crucible of Worlds | Life from the Loam | Lotus Cobra | Oracle of Mul Daya | Tireless Provisioner | Augur of Autumn
+# Payoffs
+Avenger of Zendikar | Field of the Dead | Wrenn and Seven | Ashaya, Soul of the Wild | Bane of Progress | Vorinclex, Voice of Hunger | Kozilek, Butcher of Truth | Void Winnower | Nyxbloom Ancient | Glacial Chasm
+# Fast Combo
+Demonic Consultation | Thassa's Oracle | Isochron Scepter | Dramatic Reversal | Underworld Breach | Ad Nauseam | Rocco, Cabaretti Caterer | Food Chain | Kinnan, Bonder Prodigy | Basalt Monolith
+# Finish with combat damage or poison
+Blightsteel Colossus | Satoru Umezawa | Eureka | Skithiryx, the Blight Dragon |  Craterhoof Behemoth | Triumph of the Hordes | Kamahl, Heart of Krosa | Elesh Norn, Grand Cenobite | Najeela, the Blade-Blossom | Yuriko, the Tiger's Shadow
+# White
+Land Tax | Tithe | Weathered Wayfarer | Gift of Estates | Deep Gnome Terramancer | Archaeomancer's Map | 
+Ankh of Mishra | Zo-zu the Punisher | Tunnel Ignus| Anethemancer | Price of Progress | Ruination | Sire of Stagnation | 
+Root Maze | Thalia Heretic Cathar | Stranglehold | Ashiok Dream Render | Yasharn Implacable Earth | Blood Moon | Magus of the Moon | Back to Basics
+Opposition Agent
+Deathrite Shaman | Rest in Peace | Dauthi Voidwalker | Bojuka Bog 
+# Rule of Law
+Rule of Law | Arcane Laboratory | Archon of Emeria | Eidolon of Rhetoric | Ethersworn Canonist | Deafening Silence
+# Land Destruction / Stax
+Armageddon | Ravages of War | Living Plane | Elesh Norn, Grand Cenobite | Cataclysm | Wake of Destruction | Catastrophe | Impending Disaster | Winter Orb
+`;
