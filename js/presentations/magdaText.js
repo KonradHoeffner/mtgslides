ba2637b72a6d730b46a92c462bb6092b067a716c
Konrad Höffner <konrad.hoeffner@uni-leipzig.de>
2022-09-03 11:44:04 +0200

    Finish Magda.

--- js/presentations/magdaText.js
+++ js/presentations/magdaText.js
@@ -1,3 +1,36 @@
 var magdaText = `Magda, Brazen Outlaw
 # https://www.moxfield.com/decks/kVVCJoXx30aEAmQ0CdDuVg
+# cEDH Iraruel https://www.moxfield.com/decks/xmH0i1HMHUi2iCd279RYPA
+# cEDH CzoWavey https://www.moxfield.com/decks/7iWdci_xRkitnDeTZp0raA
+# monored discord https://discord.com/invite/GPfQsY2mud
+# 8 1 mana dwarves + Universal Automaton + Dwarven Pony
+Bloodfire Dwarf | Dwarven Armorer | Dwarven Grunt | Dwarven Scorcher | Dwarven Trader | Enslaved Dwarf | Liberated Dwarf | Spark Mage | Universal Automaton | Dwarven Pony
+# 16 2 mana dwarves, 7 old border, 9 new border including Magda
+Dwarven Blastminer | Dwarven Miner | Dwarven Weaponsmith | Dwarven Berserker |  Pardic Miner | Dwarven Lieutenant | Dwarven Soldier
+Seven Dwarves | Rimrock Knight | Vault Robber | Electrostatic Infantry | Fearless Liberator | Insufferable Balladeer | Axgard Cavalry | Brazen Dwarf
+# 14 3 mana, 3 new border, 11 old + Taurean Mauler
+Dwarven Bloodboiler | Dwarven Vigilantes | Dwarven Sea Clan | Dwarven Thaumaturgist | Dwarven Nomad | Dwarven Warriors | Dwarven Recruiter | Balthor the Stout | Dwarven Demolition Team | Pardic Swordsmith | Dwarven Patrol
+Dwarven Patrol | Plundering Barbarian | Breakneck Berserker | Twinscroll Shaman | Taurean Mauler
+# Artifact Dwarves
+Universal Automaton | Proteus Machine | Adaptive Automaton | Metallic Mimic | Bloodline Pretender
+# Dragons
+Utvara Hellkite | Bogardan Hellkite | Goldspan Dragon | Ancient Copper Dragon | Shivan Hellkite | Worldgorger Dragon | Hellkite Tyrant | Gadrak, the Crown-Scourge | Atsushi, the Blazing Sky | Hellkite Charger
+# Tutor Artifacts
+Memory Jar | Null Brooch |  Unlicensed Hearse | Soul-Guide Lantern | Kaldra Compleat | Wurmcoil Engine | Academy Manufactor | Defense Grid | God-Pharaoh's Statue | Clock of Omens
+# Combo
+Clock of Omens | Liquimetal Torque | Maskwood Nexus | Xorn | Academy Manufactor | Lion's Eye Diamond | Elixir of Immortality | Memory Jar | Defense Grid  | Shivan Hellkite
+# interaction
+Red Elemental Blast | Pyroblast | Burnout | | Deflecting Swat | Lightning Bolt | Chain Lightning | Stoke the Flames | Pyrokinesis | Mizzium Mortars | Spine of Ish Sah  
+# anti artifact
+Abrade | Shattering Spree | Mogg Salvage | Vandalblast | Karn, the Great Creator | Liquimetal Torque | Liquimetal Coating | Ashnod's Transmogrant | Humility | Elesh Norn, Grand Cenobite
+# vehicles
+Smuggler's Copter | High-Speed Hoverbike | Unlicensed Hearse | Dermotaxi | Mechtitan Core | Reckoner Bankbuster | Mighty Servant of Leuk-o | Hoard Hauler | Sky Skiff | Gilded Assault Cart
+# card draw or advantage
+Memory Jar | Wheel of Fortune | Professional Face-Breaker | Academy Manufactor | Ragavan, Nimble Pilferer | Warp World | Karn, the Great Creator | Goblin Welder | Goblin Engineer | Daretti, Scrap Savant
+# lands
+Ruination | Mountain | Mutavault | Homeward Path | Dwarven Mine | Ancient Tomb | Gemstone Caverns | Cavern of Souls | Barbarian Ring | Blast Zone
+Holdout Settlement | Shatterskull Smashing | Shinka, the Bloodsoaked Keep | Survivor's Encampment | Treasure Vault | Urza's Saga | Inventor's Fair | Arid Mesa | Snow-Covered Mountain | Strip Mine
+# ramp
+Sol Ring | Mana Crypt | Mox Amber | Mox Opal | Springleaf Drum | Ruby Medallion | Jeska's Will | Professional Face-Breaker
+Dockside Extortionist | Jeweled Lotus | Chrome Mox | Mox Diamond | Lotus Petal | Ragavan, Nimble Pilferer | Simian Spirit Guide | Rite of Flame | Mana Vault
 `;
