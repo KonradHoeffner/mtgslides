010ab1a9f78c09de8c0dac8a9252567aeae2e5ab
Konrad Höffner <konrad.hoeffner@uni-leipzig.de>
2022-09-18 09:36:00 +0200

    Start new 2022.

---
+++ js/presentations/new2022.js
@@ -0,0 +1,39 @@
+// new cards from 2021 to September 2022
+// 2019- may 2021 in new2021 video
+// bans: hullbreacher
+// Regeländerungen: nur neue Mechaniken wie Dungeons und Hintergrund, die aber auf den Karten draufstehen
+// already covered in last video from 2021: Kaldheim in January, Strixhaven in April
+// Dungeons & Dragons Abenteuer in den Vergessenen Reichen 2021 Jul
+// Innistrad: Mitternachtsjagd 2021 Sep
+// Innistrad: Blutroter Bund 2021 Nov
+// Kamigawa: Neon-Dynastie 2022 Feb
+// Straßen von Neu-Capenna 2022 April
+// Commander Legends: Schlacht um Baldurs Gate 2022 Jun
+// Double Masters 2022 July Reprints
+// Dominaria: United 2022 Sep
+// Warhammer 40000 Commander Decks 2022 Oct
+var new2022 = `
+# Lands
+## Kamigawa Neon Dynasty legendary channel lands
+## interaktiv gut für das spiel gegen flood/screw und gegen counterspells aber teuer 
+Boseiju, Who Endures | Otawara, Soaring City | Eiganjo, Seat of the Empire | Takenuma, Abandoned Mire | Sokenzan, Crucible of Defiance
+# old
+# Other
+Prismatic Vista | Emergence Zone
+# Artefakte
+Arcane Signet | Talisman of Curiosity | Talisman of Hierarchy | Talisman of Resilience | Talisman of Conviction | Jeweled Lotus
+# Rot
+Dockside Extortionist | Deflecting Swat | Jeska's Will
+Underworld Breach | Lion's Eye Diamond | Brainfreeze | Intuition | Sevinne's Reclamation
+# Weiß
+Smothering Tithe | Drannith Magistrate | Ranger Captain of Eos
+# Blau
+# Schwarz
+Opposition Agent | Wishclaw Talisman | Peer into the Abyss | Bolas's Citadel
+# Grün
+Veil of Summer | Finale of Devastation | Collector Ouphe | Null Rod | Force of Vigor | Destiny Spinner
+# Gold
+Koma, Cosmos Serpent
+`;
+/*
+
