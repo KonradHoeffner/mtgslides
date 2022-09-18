525e0654af9972fec6a79efd7045f1e13c625d56
Konrad Höffner <konrad.hoeffner@uni-leipzig.de>
2022-09-18 23:00:39 +0200

    More new 2022.

--- js/presentations/new2022.js
+++ js/presentations/new2022.js
@@ -3,6 +3,7 @@
 // bans: hullbreacher
 // Regeländerungen: nur neue Mechaniken wie Dungeons und Hintergrund, die aber auf den Karten draufstehen
 // already covered in last video from 2021: Kaldheim in January, Strixhaven in April
+// Modern Horizons 2 2021 June
 // Dungeons & Dragons Abenteuer in den Vergessenen Reichen 2021 Jul
 // Innistrad: Mitternachtsjagd 2021 Sep
 // Innistrad: Blutroter Bund 2021 Nov
@@ -16,7 +17,27 @@ var new2022 = `
 # Lands
 ## Kamigawa Neon Dynasty legendary channel lands
 ## interaktiv gut für das spiel gegen flood/screw und gegen counterspells aber teuer 
-Boseiju, Who Endures | Otawara, Soaring City | Eiganjo, Seat of the Empire | Takenuma, Abandoned Mire | Sokenzan, Crucible of Defiance
+#Boseiju, Who Endures | Otawara, Soaring City | Eiganjo, Seat of the Empire | Takenuma, Abandoned Mire | Sokenzan, Crucible of Defiance
+
+# Colorless
+Urza's Saga | Plaza of Heroes | Treasure Vault | Academy Manufactor | Void Mirror | Moonsilver Key | Pyre of Heroes | Weathered Runestone | Containment Construct |  Ornithopter of Paradise
+Sword of Hearth and Home | Kaldra Compleat | Relic of Legends | Maskwood Nexus | Bloodline Pretender | Liquimetal Torque | Unlicensed Hearse | Dermotaxi | Mighty Servant of Leuk-o | Reckoner Bankbuster
+Luxior, Giada's Gift | Prized Statue | Roadside Reliquary
+#| Circuit Mender | Currency Converter | Dollhouse of Horrors | Eye of Vecna | Leather Armor | Myr Scrapling | Shrine Steward
+
+# White
+Esper Sentinel | Archivist of Oghma | Eiganjo, Seat of the Empire | Cathar Commando | Outland Liberator | Frenzied Trapbreaker | Solitude | March of Otherworldly Light | Portable Hole | Deep Gnome Terramancer
+Paladin Class | Swift Reconfiguration | Moon-Blessed Cleric | Hallowed Haunting | Out of Time | Lion Sash | Sanctifier en-Vec |  Oswald Fiddlebender | Abdel Adrian, Gorion's Ward | Touch the Spirit Realm
+Vanquish the Horde | Adeline, Resplendent Cathar | Aerial Extortionist | Ingenious Smith | Search for Glory | Smuggler's Share | Strict Proctor | The Wandering Emperror
+# The Restoration of Eiganjo | Architect of Restoration
+
+# Blue
+Otawara, Soaring City | An Offer You Can't Refuse | Ledger Shredder | March of Swirling Mist | Solve the Equation | Dress Down | Resculpt | Alchemist's Retrieval | Hullbreaker Horror | Displacer Kitten 
+Step Through | Imposter Mech | Archmage Emeritus | Consider | Geistwave | Grazilaxx, Illithid Scholar | Moonsnare Prototype | Suspend | Mystic Reflection | Otherworldly Gaze
+The Reality Chip | You Come to a River | Aether Channeler | Alrund's Epihany | Cut Your Losses | Defiler of Dreams | Fading Hope | Gale, Waterdeep Prodigy | Jin-Gitaxias, Progress Tyrant | Micromancer
+Slip Out the Back | Moon-Circuit Hacker | Thousand Faced Shadow | Prosperous Thief | Winged Boots | Witness Protection | Volshe Tideturner
+
+# Green
 # old
 # Other
 Prismatic Vista | Emergence Zone
@@ -35,5 +56,3 @@ Veil of Summer | Finale of Devastation | Collector Ouphe | Null Rod | Force of V
 # Gold
 Koma, Cosmos Serpent
 `;
-/*
-
