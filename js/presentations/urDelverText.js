de4bf587eec9f8e42319260f4965ee0bc5c04d44
Konrad Höffner <konrad.hoeffner@uni-leipzig.de>
2023-02-22 19:20:16 +0100

    More UR Delver.

--- js/presentations/urDelverText.js
+++ js/presentations/urDelverText.js
@@ -2,8 +2,9 @@ var urDelverText = `Delver of Secrets | Dragon's Rage Channeler
 Volcanic Island*4 | Steam Vents | Polluted Delta*4 | Flooded Strand*4 | Wasteland*4 | Mystic Sanctuary | Island | Delver of Secrets*4 | Dragon's Rage Channeler*4 | Murktide Regent*4 | Force of Will*4 | Daze*4 | Ponder*4 | Brainstorm*4 | Expressive Iteration*4| Mishra's Bauble*2 | Lightning Bolt*4 | Brazen Borrower
 Pyroblast | Unholy Heat | Chain Lightning | Fury | Mishra's Bauble | Counterbalance | Force of Negation | Brazen Borrower | Mercurial Spelldancer | Ledger Shredder
 Stifle | Spell Pierce | Spell Snare | Young Pyromancer | Third Path Iconoclast
-Volcanic Island*4 | Steam Vents | Polluted Delta*4 | Flooded Strand*4 | Wasteland*4 | Mystic Sanctuary | Island | Delver of Secrets*4 | Dragon's Rage Channeler*4 | Murktide Regent*4 | Force of Will*4 | Daze*4 | Ponder*4 | Brainstorm*4 | Expressive Iteration*4| Mishra's Bauble*2 | Lightning Bolt*4 | Brazen Borrower | Counterbalance| Unholy Heat
-Volcanic Island*4 | Steam Vents | Polluted Delta*4 | Flooded Strand*4 | Wasteland*4 | Mystic Sanctuary | Island | Delver of Secrets*4 | Dragon's Rage Channeler*4 | Murktide Regent*2 | Force of Will*4 | Daze*4 | Ponder*4 | Brainstorm*4 | Expressive Iteration*4| Mishra's Bauble*2 | Pyroblast*4 | Brazen Borrower | Counterbalance | Hydroblast | Force of Negation*2
+Volcanic Island*4 | Steam Vents | Polluted Delta*4 | Flooded Strand*4 | Wasteland*4 | Mystic Sanctuary | Island | Delver of Secrets*4 | Dragon's Rage Channeler*4 | Murktide Regent*4 | Force of Will*4 | Daze*4 | Ponder*4 | Brainstorm*4 | Expressive Iteration*4| Mishra's Bauble*2 | Lightning Bolt*4 | Brazen Borrower | Minor Misstep | Unholy Heat
+# vs Sneak and Show
+Volcanic Island*4 | Steam Vents | Polluted Delta*4 | Flooded Strand*4 | Wasteland*4 | Mystic Sanctuary | Hydroblast | Delver of Secrets*4 | Dragon's Rage Channeler*4 | Murktide Regent*2 | Force of Will*4 | Daze*4 | Ponder*4 | Brainstorm*4 | Expressive Iteration*4| Mishra's Bauble*2 | Pyroblast*4 | Brazen Borrower | Minor Misstep | Counterbalance | Force of Negation*2
 Volcanic Island*4 | Steam Vents | Polluted Delta*4 | Flooded Strand*4 | Wasteland*4 | Mystic Sanctuary | Island | Delver of Secrets*4 | Dragon's Rage Channeler*4 | Murktide Regent*3 | Force of Will*3 | Daze*4 | Ponder*4 | Brainstorm*4 | Expressive Iteration*4| Mishra's Bauble*2 | Lightning Bolt*4 | Pyrokinesis | Rough | Unholy Heat | Submerge | End the Festivities
 Volcanic Island | Delver of Secrets | Daze | Wasteland | Flooded Strand | Force of Will | Lightning Bolt
 Volcanic Island | Volcanic Island | Scalding Tarn | Show and Tell | Sneak Attack | Ponder | Brainstorm
@@ -14,4 +15,13 @@ Force of Will*4 | Mishra's Bauble		| Pyroblast | Hydroblast | Counterbalance | U
 # Shadow
 Thoughtseize*4 | Grief*4 | Snuff Out	| Unlicensed Hearse*2 | Hydroblast | Blue Elemental Blast | Brazen Borrower | Fatal Push*2 
 Wasteland*4 | Mishra's Bauble | Pyroblast | Counterbalance | Price of Progress | Unlicensed Hearse | True-Name Nemesis | Maddening Hex | Izzet Staticaster
+
+# Random Hands
+Flooded Strand | Brainstorm | Brainstorm | Ponder | Daze | Force of Will | Dragon's Rage Channeler
+#Volcanic Island | Island | Flooded Strand
+Flooded Strand | Flooded Strand | Flooded Strand | Volcanic Island | Delver of Secrets | Daze | Expressive Iteration
+Volcanic Island | Ponder | Daze | Daze | Murktide Regent | Lightning Bolt | Expressive Iteration
+Polluted Delta | Flooded Strand | Volcanic Island | Daze | Force of Will | Lightning Bolt | Dragon's Rage Channeler
+Island | Wasteland | Mishra's Bauble | Volcanic Island | Brainstorm | Brazen Borrower | Force of Will
+Mystic Sanctuary | Flooded Strand | Brainstorm | Minor Misstep | Brazen Borrower | Murktide Regent | Dragon's Rage Channeler
 `;
