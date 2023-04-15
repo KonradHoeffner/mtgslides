195aa455ce1371df87daea1eaf90081b57c7b704
Konrad Höffner <konrad.hoeffner@uni-leipzig.de>
2023-04-15 17:47:10 +0200

    Start Legacy ANT.

---
+++ js/presentations/antText.js
@@ -0,0 +1,73 @@
+var antText = `Ad Nauseam | Tendrils of Agony
+# Natural Tendrils
+Dark Ritual | Dark Ritual | Cabal Ritual | Lotus Petal | Thoughtseize | Duress | Tendrils of Agony
+# Tutors
+Infernal Tutor*4 | Underground Sea*15
+# Tutor Chain
+Underground Sea | Underground Sea | Cabal Ritual | Cabal Ritual | Cabal Ritual | Dark Ritual | Duress | Infernal Tutor*4 | Tendrils of Agony
+# Tutors + LED
+Infernal Tutor | Lion's Eye Diamond 
+# Engines
+Ad Nauseam | Past in Flames | Pair o' Dices Lost | Peer into the Abyss
+Ad Nauseam | Lotus Petal | Draco
+Swamp | Dark Ritual | Dark Ritual | Ad Nauseam | Lotus Petal | Dark Ritual | Lion's Eye Diamond | Infernal Tutor
+Past in Flames | Swamp | Dark Ritual | Cabal Ritual | Infernal Tutor | Tendrils of Agony
+Pair o'Dice Lost | Swamp | Dark Ritual | Cabal Ritual | Infernal Tutor | Tendrils of Agony
+# sum of two dice
+# in 1/36. 2: 1, 3: 2, 4: 3, 5: 4, 6: 5, 7: 6, 8: 5
+# at least. 2: 36, 3: 35, 4: 32, 5: 29, 6: 25, 7: 20, 8: 14, 9: 9, 10: 5, 11: 2, 12: 1
+# in percent at least. 2: 100, 3: 97.2, 4: 88.9, 5: 80.6, 6: 69.4, 7: 55.6, 8: 38.88, 9: 25, 10: 13.9, 11: 5.6, 12: 2.8
+Pair o' Dice Lost | Swamp | Lotus Petal | Lion's Eye Diamond | Lion's Eye Diamond | Wishclaw Talisman | Tendrils of Agony
+
+# PODL/PIF + IT + 3 cards
+# by Cave from the Storm Discord
+# only rituals, requires hellbent
+#IT+DR DR DR = PIF, BRh, !
+Dark Ritual | Dark Ritual | Dark Ritual | Infernal Tutor
+Swamp | Volcanic Island | Dark Ritual*3 | Infernal Tutor | Past in Flames | Dark Ritual*3 | Infernal Tutor | Tendrils of Agony | Swamp | Swamp
+# roll 4 for 18 89%, 5+ for 22 80$
+Swamp | Forest*2 | Dark Ritual*3 | Infernal Tutor | Pair o' Dice Lost | Dark Ritual*3 | Infernal Tutor*2 | Tendrils of Agony
+Swamp | Forest*2 | Dark Ritual*3 | Infernal Tutor | Pair o' Dice Lost | Dark Ritual*2 | Infernal Tutor | Tendrils of Agony
+
+#IT+DR DR CR = PIF, BRh, !,
+Dark Ritual | Dark Ritual | Cabal Ritual | Infernal Tutor
+Swamp | Volcanic Island | Dark Ritual*2 | Cabal Ritual | Infernal Tutor | Past in Flames | Dark Ritual*2 | Cabal Ritual | Infernal Tutor | Tendrils of Agony
+# no threshold , roll 6+ 69%
+Swamp | Forest*2 | Dark Ritual*2 | Cabal Ritual | Infernal Tutor | Pair o' Dice Lost | Dark Ritual*2 | Cabal Ritual | Infernal Tutor | Tendrils of Agony
+# threshold, roll 4 for 16
+Swamp | Forest*2 | Dark Ritual*2 | Cabal Ritual | Infernal Tutor | Pair o' Dice Lost | Cabal Ritual | Infernal Tutor | Tendrils of Agony
+# threshold, roll 5 for 20
+Swamp | Forest*2 | Dark Ritual*2 | Cabal Ritual | Infernal Tutor | Pair o' Dice Lost | Dark Ritual | Cabal Ritual | Infernal Tutor*2 | Tendrils of Agony
+# threshold, roll 6+ for 24
+Swamp | Forest*2 | Dark Ritual*2 | Cabal Ritual | Infernal Tutor | Pair o' Dice Lost | Dark Ritual*2 | Cabal Ritual | Infernal Tutor*3 | Tendrils of Agony
+
+#IT+CR CR DR = PIF, BRht(2), !
+Dark Ritual | Cabal Ritual | Cabal Ritual | Infernal Tutor
+# pif thresh same as dr*3
+
+#IT+DR DR LP = PIF, 2B/Rsh, ?
+#IT+DR DR LED = PIF, Bs, !!
+#IT+CR CR CR = PIF, 1BRht(3), ?
+#IT+CR CR LP = PIF, 2B/Rsht(2), ?
+#IT+CR CR LED = PIF, 1Bst(2), !
+#IT+LP LP LP = PL, 7h, ??
+#IT+LP LP LED = PL, 3, !
+#IT+LP LP DR = PL, 3B/GB/Ghd(3), ??
+#IT+LP LP CR = PL, 1Bht(1)d(4), ?
+#IT+LED LED LED = PL, 1B, !
+#IT+LED LED LP = PL, 2, !!
+#IT+LED LED DR = PL, Bd(3), !!,
+#IT+LED LED CR = PL, 1Bt(1)d(4), !
+#IT+DR CR LP = PIF, Bsht(1), !
+#IT+DR CR LED = PIF, Bst(2), !
+#IT+DR LP LED = PL, Bd(3), !!
+#IT+CR LP LED = PL, 1Bt(1)d(4), !
+
+# Cantrips
+Brainstorm | Ponder | Preordain
+# Alternative Win Condition
+Empty the Warrens | Aeve, Progenitor Ooze
+
+# Lands
+# Random Hands
+`;
