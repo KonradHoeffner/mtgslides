159bc2685e87f76e9afc148bee93fe8a9d35eb40
Konrad Höffner <konrad.hoeffner@uni-leipzig.de>
2023-05-07 12:44:21 +0200

    start cephalid breakfast

---
+++ js/presentations/cephalidText.js
@@ -0,0 +1,56 @@
+var cephalidText = `Cephalid Illusionist | Nomads en-Kor
+# Combo
+Cephalid Illusionist | Nomads en-Kor | Shuko | Narcomoeba*3 | Cabal Therapy | Dread Return | Thassa's Oracle
+# Rules
+Nomads en-Kor
+Shuko
+#Ponder*4 | Brainstorm*4 | Stoneforge Mystic*3 | Urza's Saga*3 | Shuko*2 | Step Through*2 | Cephalid Illusionist*4 | Thassa's Oracle
+Ponder | Brainstorm | Stoneforge Mystic | Urza's Saga | Shuko | Step Through | Cephalid Illusionist | Thassa's Oracle
+Force of Will | Daze | Teferi, Time Raveler | Cabal Therapy | Orim's Chant | Flusterstorm
+Urza's Saga | Stoneforge Mystic | Kaldra Compleat | Narcomoeba | Shuko
+Aether Vial | Baleful Strix | Grand Abolisher | Staff of the Storyteller | Retrofitter Foundry | Lotus Petal | 
+
+# Niccolò Olivieri
+Tundra*2 | Underground Sea | Scrubland | Island*3 | Plains | Flooded Strand*4 | Polluted Delta*4 | Marsh Flats | Urza's Saga*3 | Cephalid Illusionist*4 | Nomads en-Kor*3 | Thassa's Oracle | Narcomoeba*3 | Stoneforge Mystic*3 | Shuko*2 | Kaldra Compleat | Brainstorm*4 | Ponder*4 | Force of Will*4 | Daze*3 | Flusterstorm | Step Through*2 | Cabal Therapy | Dread Return | Teferi, Time Raveler*3
+Tundra*3 | Underground Sea | Underground Sea | Island*2 | Plains | Flooded Strand*4 | Polluted Delta*4 | Karakas | Urza's Saga*3 | Cephalid Illusionist*4 | Nomads en-Kor*3 | Thassa's Oracle | Narcomoeba*3 | Stoneforge Mystic*3 | Shuko*2 | Kaldra Compleat | Brainstorm*4 | Ponder*4 | Force of Will*4 | Daze*3 | Flusterstorm | Step Through*2 | Cabal Therapy | Dread Return | Teferi, Time Raveler*3
+
+# Sideboard
+Swords to Plowshares*4 | Prismatic Ending*4 | Force of Negation*2 | Flusterstorm | Surgical Extraction*2 | Serenity*2
+# Alternatives
+Hydroblast | Blue Elemental Blast | Thassa's Oracle | Jack-o'-Lantern | Memory's Journey | Soul-Guide Lantern | Pithing Needle | Plague Engineer | Thoughtseize | Brazen Borrower
+
+# Matchups and Sideboarding
+## Combo
+### TES
+Chrome Mox | Rite of Flame | Ad Nauseam | Echo of Eons | Galvanic Relay |  Burning Wish | Tendrils of Agony | Empty the Warrens | Orim's Chant | Veil of Summer
+Galvanic Relay*3 | Thoughtseize*3
+
+Deafening Silence | Lavinia, Azorius Renegade | Force of Negation | Flusterstorm | Orim's Chant | Spell Pierce | Thoughtseize | Surgical Extraction | Hydroblast | Blue Elemental Blast
+Stoneforge Mystic | Kaldra Compleat | Aether Vial | Baleful Strix | Swamp
+
+Force of Negation | Force of Negation | Flusterstorm | Surgical Extraction | Surgical Extraction | Stoneforge Mystic | Kaldra Compleat | Teferi, Time Raveler | Teferi, Time Raveler | Teferi, Time Raveler
+
+# Sample Hands
+Brainstorm | Cabal Therapy | Step Through | Dread Return | Daze | Flooded Strand | Stoneforge Mystic
+Flooded Strand | Polluted Delta | Ponder | Stoneforge Mystic | Force of Will | Polluted Delta | Cephalid Illusionist
+Narcomoeba | Daze | Brainstorm | Underground Sea | Flooded Strand | Stoneforge Mystic | Force of Will
+
+### ANT
+Dark Ritual | Cabal Ritual | Lotus Petal | Lion's Eye Diamond |  Infernal Tutor | Wishclaw Talisman | Past in Flames | Ad Nauseam | Pair o' Dice Lost | Tendrils of Agony
+Duress | Thoughtseize | Veil of Summer | Cabal Therapy | Orim's Chant | Daze 
+Veil of Summer | Carpet of Flowers | Abrupt Decay | Surgical Extraction | Massacre | Flusterstorm | Dress Down | Chain of Vapor | Containment Priest
+
+Force of Negation | Force of Negation | Flusterstorm | Surgical Extraction | Surgical Extraction | Stoneforge Mystic | Kaldra Compleat | Teferi, Time Raveler | Teferi, Time Raveler | Karakas
+
+# Sample Hands
+Brainstorm | Cabal Therapy | Step Through | Dread Return | Daze | Flooded Strand | Stoneforge Mystic
+Flooded Strand | Polluted Delta | Ponder | Stoneforge Mystic | Force of Will | Polluted Delta | Cephalid Illusionist
+Narcomoeba | Daze | Brainstorm | Underground Sea | Flooded Strand | Stoneforge Mystic | Force of Will
+
+# Cabal Therapy
+# Faery Macabre
+# Multiple
+Cephalid Illusionist | Nomads en-Kor | Thassa's Oracle | Step Through
+Narcomoeba | Cabal Therapy | Dread Return | Thassa's Oracle
+Teferi, Time Raveler | Cabal Therapy | Fearie Macabre | Dread Return | Thassa's Oracle | Fearie Macabre
+`;
