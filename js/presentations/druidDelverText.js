a21735855a089bcf9c1cd0444c5e39a3c354af63
Konrad Höffner <konrad.hoeffner@uni-leipzig.de>
2023-10-01 13:00:58 +0200

    add Questing Druid Temur Delver

---
+++ js/presentations/druidDelverText.js
@@ -0,0 +1,47 @@
+var druidDelverText = `Delver of Secrets | Dragon's Rage Channeler | Questing Druid
+Questing Druid | Expressive Iteration | Quirion Dryad | Quicken | Borrowed Time | Tropical Island | Delver of Secrets | Mystic Sanctuary | Lightning Bolt | Volcanic Island*2
+# Core 56
+Volcanic Island*3 |Tropical Island*2 | Polluted Delta*4 | Flooded Strand*4 | Wasteland*4 | Delver of Secrets*3 | Dragon's Rage Channeler*4 | Murktide Regent*3 | Questing Druid*3 | Force of Will*4 | Daze*4 | Ponder*4 | Brainstorm*4 | Mishra's Bauble*4 | Lightning Bolt*4
+Volcanic Island*3 |Tropical Island*2 | Polluted Delta*4 | Flooded Strand*4 | Wasteland*4 | Delver of Secrets*4 | Dragon's Rage Channeler*4 | Murktide Regent*3 | Questing Druid*4 | Force of Will*4 | Daze*4 | Ponder*4 | Brainstorm*4 | Mishra's Bauble*4 | Brazen Borrower | Lightning Bolt*4 | Chain Lightning
+
+# Good Matchups
+Show and Tell | Doomsday | High Tide | Cephalid Illusionist | Riddlesmith
+#| Balustrade Spy | 
+
+# Bad Matchups
+Mox Diamond | Dark Depths | Green Sun's Zenith
+
+# Somewhat Even Matchups
+#Delver of Secrets | Ad Nauseam | Reanimate | Sai, Master Thopterist | Painter's Servant
+
+# Difference to Grixis Delver
+# Worse Matchups
+Glimpse of Nature | Brainstorm | Echo of Eons | High Tide | Riddlesmith | Sai, Master Thopterist | Flickerwisp | Echo of Eons
+# Better Matchups
+Mox Diamond | Dark Depths | Helm of Obedience
+
+# Playstyle
+Ponder | Brainstorm | Volcanic Island | Tropical Island | Questing Druid | Daze | Force of Will | Lightning Bolt | Brazen Borrower | Murktide Regent
+Show and Tell | Seek the Beast | Daze | Force of Will
+Daze | Seek the Beast | Volcanic Island | Ponder
+Volcanic Island | Volcanic Island | Tropical Island | Tropical Island | Seek the Beast | Questing Druid | Ponder | Brainstorm
+Volcanic Island*2 | Tropical Island*2 | Seek the Beast | Ponder | Lightning Bolt
+
+# Tuning Mainboard
+Unholy Heat | Forked Bolt | Counterbalance | Spell Pierce | Minor Misstep | Delver of Secrets | Questing Druid | Chain Lightning | Brazen Borrower
+Unholy Heat | Troll of Khazad-dûm | Murktide Regent | Death's Shadow | Knight of the Reliquary
+Minor Misstep | Dark Ritual | Death's Shadow | Swords to Plowshares | Chalice of the Void
+Counterbalance | Tendrils of Agony | Delver of Secrets | Snapcaster Mage
+Spell Pierce | Show and Tell | Doomsday | Teferi, Time Raveler | Narset, Parter of Veils | Minsc & Boo, Timeless Heroes
+
+# Sideboard Cards
+Pyroblast*3 | Hydroblast | Force of Negation*2 | Null Rod | Grafdigger's Cage | Soul-Guide Lantern | Counterbalance | Meltdown*2 | End the Festivities | Minsc & Boo | Sylvan Library
+Carpet of Flowers | Delver of Secrets | Death's Shadow
+Veil of Summer | Grief | Thoughtseize | Orcish Bowmaster | Tendrils of Agony
+Surgical Extraction | Reanimate | Exhume | Doomsday | Life from the Loam | Bridge from Below | Past in Flames | Uro, Titan of Nature's Wrath | Goblin Welder | Dread Return
+Flusterstorm | Show and Tell | Doomsday | Ad Nauseam | Reanimate | Balustrade Spy | Glistener Elf | Mind's Desire | Force of Will | Swords to Plowshares
+Maddening Hex | Dark Ritual | Death's Shadow | Snapcaster Mage
+
+# Verdict
+Fiery Conclusion | Expressive Iteration | Orcish Bowmaster | Delver of Secrets | Uro, Titan of Nature's Wrath
+`;
