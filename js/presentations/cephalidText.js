a0aaf82f79baaf25134f7cc9e827076e0fb8470e
Konrad Höffner <konrad.hoeffner@uni-leipzig.de>
2024-01-13 20:52:33 +0100

    more breakfast

--- js/presentations/cephalidText.js
+++ js/presentations/cephalidText.js
@@ -5,15 +5,16 @@ Cephalid Illusionist | Nomads en-Kor | Shuko | Narcomoeba*3 | Cabal Therapy | Dr
 Nomads en-Kor
 Shuko
 # Core 49
-Tundra*2 | Underground Sea | Island | Plains | Flooded Strand*4 | Polluted Delta*2 | Misty Rainforest*2 | Urza's Saga*3 | Cephalid Illusionist*4 | Nomads en-Kor*3 | Shuko*2 | Narcomoeba*3 | Cabal Therapy | Dread Return | Thassa's Oracle | Step Through*2 | Brainstorm*4 | Ponder*4 | Force of Will*4 | Daze*2 | Teferi, Time Raveler*2
+Tundra*2 | Underground Sea | Snow-Covered Island |  Snow-Covered Plains | Flooded Strand*4 | Polluted Delta*2 | Misty Rainforest*2 | Urza's Saga*3 | Cephalid Illusionist*4 | Nomads en-Kor*3 | Shuko*2 | Narcomoeba*3 | Cabal Therapy | Dread Return | Thassa's Oracle | Step Through*2 | Brainstorm*4 | Ponder*4 | Force of Will*4 | Daze*2 | Teferi, Time Raveler*2
 # Staple SFM 11
-Tundra | Underground Sea | Island | Scalding Tarn | Stoneforge Mystic*3 | Kaldra Compleat | Daze | Orim's Chant*2
+Tundra | Underground Sea | Snow-Covered Island | Scalding Tarn | Stoneforge Mystic*3 | Kaldra Compleat | Daze | Orim's Chant*2
 # Staple SFM 60
-Tundra*3 | Underground Sea*2 | Island*2 | Plains | Flooded Strand*4 | Polluted Delta*4 | Misty Rainforest | Urza's Saga*3 | Cephalid Illusionist*4 | Nomads en-Kor*3 | Shuko*2 | Narcomoeba*3 | Cabal Therapy | Dread Return | Thassa's Oracle | Step Through*2 | Brainstorm*4 | Ponder*4 | Force of Will*4 | Daze*3 | Teferi, Time Raveler*2 | Stoneforge Mystic*3 | Kaldra Compleat | Orim's Chant*2
+Tundra*3 | Underground Sea*2 | Snow-Covered Island*2 |  Snow-Covered Plains | Flooded Strand*4 | Polluted Delta*4 | Misty Rainforest | Urza's Saga*3 | Cephalid Illusionist*4 | Nomads en-Kor*3 | Shuko*2 | Narcomoeba*3 | Cabal Therapy | Dread Return | Thassa's Oracle | Step Through*2 | Brainstorm*4 | Ponder*4 | Force of Will*4 | Daze*3 | Teferi, Time Raveler*2 | Stoneforge Mystic*3 | Kaldra Compleat | Orim's Chant*2
+Tundra*3 | Underground Sea*2 | Snow-Covered Island*2 |  Snow-Covered Plains | Flooded Strand*4 | Polluted Delta*4 | Scrubland | Urza's Saga*3 | Cephalid Illusionist*4 | Nomads en-Kor*3 | Shuko*2 | Narcomoeba*3 | Cabal Therapy | Dread Return | Thassa's Oracle | Step Through*2 | Brainstorm*4 | Ponder*4 | Force of Will*4 | Daze*2 | Teferi, Time Raveler*2 | Orcish Bowmasters*4 | Retrofitter Foundry | Orim's Chant*2
 # Staple Sideboard
 Swords to Plowshares*4 | Prismatic Ending*2 | Force of Negation*2 | Flusterstorm*2 | Surgical Extraction*2 | Serenity*2 | Pithing Needle
 
-Tundra*2 | Underground Sea | Island | Plains | Flooded Strand*4 | Polluted Delta*2 | Misty Rainforest*2 | Urza's Saga*3 | Cephalid Illusionist*4 | Nomads en-Kor*3 | Shuko*2 | Narcomoeba*3 | Cabal Therapy | Dread Return | Thassa's Oracle | Step Through*2 | Brainstorm*4 | Ponder*4 | Force of Will*4 | Daze*2 | Teferi, Time Raveler*2
+Tundra*2 | Underground Sea | Snow-Covered Island |  Snow-Covered Plains | Flooded Strand*4 | Polluted Delta*2 | Misty Rainforest*2 | Urza's Saga*3 | Cephalid Illusionist*4 | Nomads en-Kor*3 | Shuko*2 | Narcomoeba*3 | Cabal Therapy | Dread Return | Thassa's Oracle | Step Through*2 | Brainstorm*4 | Ponder*4 | Force of Will*4 | Daze*2 | Teferi, Time Raveler*2
 
 Cephalid Illusionist*4 | Nomads en-Kor*3 | Shuko*2 | Narcomoeba*3 | Cabal Therapy | Dread Return | Thassa's Oracle | Ponder | Brainstorm
 
@@ -24,8 +25,8 @@ Urza's Saga | Stoneforge Mystic | Kaldra Compleat | Narcomoeba | Shuko
 Aether Vial | Baleful Strix | Grand Abolisher | Staff of the Storyteller | Retrofitter Foundry | Lotus Petal | 
 
 # Niccolò Olivieri
-Tundra*2 | Underground Sea | Scrubland | Island*3 | Plains | Flooded Strand*4 | Polluted Delta*4 | Marsh Flats | Urza's Saga*3 | Cephalid Illusionist*4 | Nomads en-Kor*3 | Thassa's Oracle | Narcomoeba*3 | Stoneforge Mystic*3 | Shuko*2 | Kaldra Compleat | Brainstorm*4 | Ponder*4 | Force of Will*4 | Daze*3 | Flusterstorm | Step Through*2 | Cabal Therapy | Dread Return | Teferi, Time Raveler*3
-Tundra*3 | Underground Sea | Underground Sea | Island*2 | Plains | Flooded Strand*4 | Polluted Delta*4 | Karakas | Urza's Saga*3 | Cephalid Illusionist*4 | Nomads en-Kor*3 | Thassa's Oracle | Narcomoeba*3 | Stoneforge Mystic*3 | Shuko*2 | Kaldra Compleat | Brainstorm*4 | Ponder*4 | Force of Will*4 | Daze*3 | Flusterstorm | Step Through*2 | Cabal Therapy | Dread Return | Teferi, Time Raveler*3
+Tundra*2 | Underground Sea | Scrubland | Snow-Covered Island*3 |  Snow-Covered Plains | Flooded Strand*4 | Polluted Delta*4 | Marsh Flats | Urza's Saga*3 | Cephalid Illusionist*4 | Nomads en-Kor*3 | Thassa's Oracle | Narcomoeba*3 | Stoneforge Mystic*3 | Shuko*2 | Kaldra Compleat | Brainstorm*4 | Ponder*4 | Force of Will*4 | Daze*3 | Flusterstorm | Step Through*2 | Cabal Therapy | Dread Return | Teferi, Time Raveler*3
+Tundra*3 | Underground Sea | Underground Sea | Snow-Covered Island*2 |  Snow-Covered Plains | Flooded Strand*4 | Polluted Delta*4 | Karakas | Urza's Saga*3 | Cephalid Illusionist*4 | Nomads en-Kor*3 | Thassa's Oracle | Narcomoeba*3 | Stoneforge Mystic*3 | Shuko*2 | Kaldra Compleat | Brainstorm*4 | Ponder*4 | Force of Will*4 | Daze*3 | Flusterstorm | Step Through*2 | Cabal Therapy | Dread Return | Teferi, Time Raveler*3
 
 # Alternatives
 Hydroblast | Blue Elemental Blast | Thassa's Oracle | Jack-o'-Lantern | Memory's Journey | Soul-Guide Lantern | Pithing Needle | Plague Engineer | Thoughtseize | Brazen Borrower
