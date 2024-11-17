8b7baca6e7242e2d3913739b39e46b2f19a5c2a3
Konrad Höffner <konrad.hoeffner@uni-leipzig.de>
2024-11-17 13:02:50 +0100

    start Legacy tempo Doomsday DDFT slides

---
+++ js/presentations/tempoDoomsdayText.js
@@ -0,0 +1,14 @@
+var tempoDoomsdayText = `Doomsday|Psychic Frog|Tamiyo, Inquisitive Student
+# All
+Underground Sea*4 | Polluted Delta*4 | Flooded Strand*4 | Swamp | Island | Undercity Sewers | Cavern of Souls | Lotus Petal*4 | Dark Ritual*4|Doomsday*4|Street Wraith*2|Edge of Autumn|Thassa's Oracle*2|Ponder*4 | Brainstorm*4 | Force of Will*4 | Daze*4 | Fatal Push*2 | Psychic Frog*4|Tamiyo, Inquisitive Student*4
+# Mana
+Underground Sea*4 | Polluted Delta*4 | Flooded Strand*4 | Swamp | Island | Undercity Sewers | Cavern of Souls | Lotus Petal*4 | Dark Ritual*4
+Ponder*4 | Brainstorm*4 | Force of Will*4 | Daze*4 | Fatal Push*2 | Psychic Frog*4|Tamiyo, Inquisitive Student*4
+Doomsday*4|Street Wraith*2|Edge of Autumn|Thassa's Oracle*2
+# Sideboard
+Barrowgoyf*3|Murktide Regent*3|Fatal Push*2|Thoughtseize|Grafdigger's Cage*2|Hydroblast|Force of Negation|Consign to Memory*2
+# Tempo in/out: Delver, BUG Beanstalk, Stiflenought
+Doomsday*4|Street Wraith*2|Edge of Autumn|Thassa's Oracle|Thassa's Oracle|Barrowgoyf*3|Murktide Regent*3|Fatal Push*2|Thoughtseize
+# Vs red
+Thoughtseize|Hydroblast
+`;
