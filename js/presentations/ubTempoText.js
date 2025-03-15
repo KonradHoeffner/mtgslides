39dcf42cb39f9c738b2617c55f69ac95a1bc6c4f
Konrad Höffner <konrad.hoeffner@uni-leipzig.de>
2025-03-15 15:57:55 +0100

    add ub tempo 2025 slides

---
+++ js/presentations/ubTempoText.js
@@ -0,0 +1,42 @@
+var ubTempoText = `Nethergoyf|Tamiyo, Inquisitive Student|Daze|Wasteland
+# CutterSlade83
+Underground Sea*4 | Polluted Delta*4 | Flooded Strand*4 | Swamp | Island | Undercity Sewers | Wasteland*4 | Nethergoyf*4| Tamiyo, Inquisitive Student*3|Orcish Bowmasters*4|Murktide Regent*2|Brazen Borrower| Ponder*4 | Brainstorm*4 | Force of Will*4 | Daze*4 | Fatal Push*4 | Thoughtseize*3 | Go for the Throat| Kaito, Bane of Nightmares | Nihil Spellbomb*2
+# Mana
+Underground Sea*4 | Polluted Delta*4 |Swamp | Island | Undercity Sewers | Flooded Strand | Misty Rainforest | Bloodstained Mire | Marsh Flats | Wasteland*4 
+# Core 51
+Nethergoyf*3| Tamiyo, Inquisitive Student*3|Orcish Bowmasters*3|Brazen Borrower| Ponder*4 | Brainstorm*4 | Force of Will*4 | Daze*4 | Fatal Push*4 | Thoughtseize*2
+# Options
+Nethergoyf*4|Tamiyo, Inquisitive Student*4|Orcish Bowmasters*4|Brazen Borrower*2|Murktide Regent*2|Barrowgoyf*2|Baleful Strix|Dauthi Voidwalker|Delver of Secrets|Thoughtseize*4 |Hymn to Tourach |Force of Negation|Go for the Throat|Shoot the Sheriff|Sheoldred's Edict|Drown in the Loch| Kaito, Bane of Nightmares | Nihil Spellbomb*2|Mishra's Bauble|Stock up
+# Konrad's baseline 55
+Orcish Bowmasters*4|Thoughtseize*3 | Kaito, Bane of Nightmares | Nihil Spellbomb
+Nethergoyf*4|Baleful Strix
+Go for the Throat|Shoot the Sheriff|Sheoldred's Edict|Brazen Borrower
+Nihil Spellbomb*2|Force of Negation|Dauthi Voidwalker
+# 3-4 big threats
+Murktide Regent*2|Barrowgoyf*2|Kaito, Bane of Nightmares*2
+
+# Example vs Delver, Storm, Painter
+Underground Sea*4 | Polluted Delta*4 | Flooded Strand*4 | Swamp | Island | Undercity Sewers | Wasteland*4 | Nethergoyf*4| Tamiyo, Inquisitive Student*3|Orcish Bowmasters*4|Barrowgoyf*1|Dauthi Voidwalker|Brazen Borrower| Ponder*4 | Brainstorm*4 | Force of Will*4 | Daze*4 | Fatal Push*4 | Thoughtseize*3 | Shoot the Sheriff | Kaito, Bane of Nightmares | Nihil Spellbomb
+
+# Sideboard
+Harbinger of the Seas*2|Eldrazi Temple|Urza's Saga|Dark Depths|Maze of Ith|Delver of Secrets|Up the Beanstalk|Carpet of Flowers|Swamp|Murktide Regent
+Consign to Memory*2|Sowing Mycospawn|Chalice of the Void|Trinisphere|The One Ring|Ensnaring Bridge|Baleful Strix|Urza's Saga|Thassa's Oracle|Animate Dead|Leyline Binding|Tendrils of Agony|Violent Outburst|Crashing Footfalls
+Barrowgoyf*2|Delver of Secrets|Lightning Bolt|Pyroblast|Up the Beanstalk|Urza's Saga|Murktide Regent|Troll of Khazad-dûm|Brainstorm|Reanimate
+Grafdigger's Cage|Reanimate|Balustrade Spy|Memory's Journey|Echo of Eons|Dread Return|Green Sun's Zenith|Natural Order|Mystic Forge|Gaea's Will|Beseech the Mirror|Ghost Vacuum
+Ghost Vacuum|Nihil Spellbomb|Surgical Extraction|Unlicensed Hearse
+Hydroblast*2|Blood Moon|Pyroblast|Fable of the Mirror-Breaker|Broadside Bombardiers|Sneak Attack|Fury|Thoughtseize|Lightning Bolt|Dragon's Rage Channeler|Pyroblast
+Force of Negation|Doomsday|Show and Tell|Sneak Attack|Beseech the Mirror|Necrodominance|Up the Beanstalk|Carpet of Flowers|Daze|Natural Order
+Sheoldred's Edict|Dark Depths|Tamiyo, Seasoned Scholar|Go for the Throat|Painter's Servant|Shoot the Sheriff|Broadside Bombardiers|Brazen Borrower|Ensnaring Bridge|Snuff Out|Troll of Khazad-dûm|Murktide Regent
+Null Rod*2|Mox Opal|Tendrils of Agony|Grim Monolith|Emry, Lurker of the Loch|Vault of Whispers
+Powder Keg|Engineered Explosives|Chalice of the Void|Urza's Saga|Vault of Whispers|Mox Opal
+Toxic Deluge*2|Gaea's Cradle|Noble Hierarch|Springheart Nantuko|Dryad Arbor|Goblin Lackey|Lord of Atlantis|Knight of the Reliquary|Flickerwisp|Ajani, Nacatl Pariah
+
+# Example Casual Creature
+Harbinger of the Seas*2|Consign to Memory*2|Barrowgoyf*2|Grafdigger's Cage|Null Rod|Hydroblast*3|Force of Negation|Sheoldred's Edict|Toxic Deluge*2
+# Example Shop Storm Painter Delver
+Harbinger of the Seas*2|Consign to Memory*2|Barrowgoyf*2|Grafdigger's Cage|Null Rod*2|Powder Keg|Hydroblast*3|Blue Elemental Blast|Force of Negation
+# Example League Trophies
+Harbinger of the Seas*2|Consign to Memory*2|Barrowgoyf*2|Grafdigger's Cage*2|Null Rod|Hydroblast*2|Force of Negation*2|Sheoldred's Edict|Toxic Deluge
+# Example Challenge CutterSlade83
+Harbinger of the Seas*2|Consign to Memory*3|Barrowgoyf*2|Grafdigger's Cage|Ghost Vacuum|Hydroblast*2|Force of Negation*2|Snuff Out|Brazen Borrower
+`;
